400eaac13902888232b7534fc0163d150eb84555
Federica <fedemoletta@hotmail.com>
2023-07-28 18:12:15 -0300

    Add tests

--- pkg/vm/memory/memory_test.go
+++ pkg/vm/memory/memory_test.go
@@ -64,3 +64,62 @@ func TestMemoryInsertWithHoles(t *testing.T) {
 		t.Errorf("Inserted value and original value are not the same")
 	}
 }
+
+func TestMemoryInsertOverWriteSameValue(t *testing.T) {
+	mem_manager := memory.NewMemorySegmentManager()
+	mem := &mem_manager.Memory
+
+	// We will insert the MaybeRelocatable Int(7) in segment 0, offset 0
+	key := mem_manager.Add()
+	val := memory.NewMaybeRelocatableInt(7)
+
+	// Make the insertion
+	err := mem.Insert(key, val)
+	if err != nil {
+		t.Errorf("Insert error in test: %s", err)
+	}
+
+	// Insert the same value again and check it doesn't fail
+	err2 := mem.Insert(key, val)
+	if err2 != nil {
+		t.Errorf("Insert error in test: %s", err)
+	}
+}
+
+func TestMemoryInsertOverWriteValue(t *testing.T) {
+	mem_manager := memory.NewMemorySegmentManager()
+	mem := &mem_manager.Memory
+
+	// We will insert the MaybeRelocatable Int(7) in segment 0, offset 0
+	key := mem_manager.Add()
+	val := memory.NewMaybeRelocatableInt(7)
+
+	// Make the insertion
+	err := mem.Insert(key, val)
+	if err != nil {
+		t.Errorf("Insert error in test: %s", err)
+	}
+
+	// Insert another value into the same address and check that it fails
+	val2 := memory.NewMaybeRelocatableInt(8)
+	err2 := mem.Insert(key, val2)
+	if err2 == nil {
+		t.Errorf("Overwritting memory value should fail")
+	}
+}
+
+func TestMemoryInsertUnallocatedSegment(t *testing.T) {
+	mem_manager := memory.NewMemorySegmentManager()
+	mem := &mem_manager.Memory
+
+	// Instantiate the address where we want to insert and the value.
+	// We will insert the value Int(5) in segment 1, offset 0
+	key := memory.NewRelocatable(1, 0)
+	val := memory.NewMaybeRelocatableInt(5)
+
+	// Make the insertion
+	err := mem.Insert(key, val)
+	if err != nil {
+		t.Errorf("Insertion on unallocated segment should fail")
+	}
+}
