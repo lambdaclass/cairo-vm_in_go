3de475ee4c34f144f4787d7695f68f221839e5d4
gabrielbosio <gabrielbosio95@gmail.com>
2023-07-31 18:06:20 -0300

    Recover memory relocation test

--- pkg/vm/memory/segments_test.go
+++ pkg/vm/memory/segments_test.go
@@ -4,6 +4,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/lambdaclass/cairo-vm.go/pkg/vm"
 	"github.com/lambdaclass/cairo-vm.go/pkg/vm/memory"
 )
 
@@ -177,5 +178,39 @@ func TestRelocateSegmentsWithHoles(t *testing.T) {
 	expectedTable := []uint{1, 4, 4}
 	if !reflect.DeepEqual(expectedTable, relocationTable) {
 		t.Errorf("Relocation tables are not the same")
+	}
+}
+
+func TestRelocateMemory(t *testing.T) {
+	virtualMachine := vm.NewVirtualMachine()
+	segments := virtualMachine.Segments
+	for i := 0; i < 4; i++ {
+		segments.AddSegment()
+	}
+	segments.Memory.Insert(memory.NewRelocatable(0, 0), memory.NewMaybeRelocatableInt(4613515612218425347))
+	segments.Memory.Insert(memory.NewRelocatable(0, 1), memory.NewMaybeRelocatableInt(5))
+	segments.Memory.Insert(memory.NewRelocatable(0, 2), memory.NewMaybeRelocatableInt(2345108766317314046))
+	segments.Memory.Insert(memory.NewRelocatable(1, 0), memory.NewMaybeRelocatableRelocatable(2, 0))
+	segments.Memory.Insert(memory.NewRelocatable(1, 1), memory.NewMaybeRelocatableRelocatable(3, 0))
+	segments.Memory.Insert(memory.NewRelocatable(1, 5), memory.NewMaybeRelocatableInt(5))
+
+	segments.ComputeEffectiveSizes()
+
+	relocationTable, ok := segments.RelocateSegments()
+	if !ok {
+		t.Errorf("Could not create relocation table")
+	}
+
+	relocatedMemory, err := segments.RelocateMemory(&relocationTable)
+	if err != nil {
+		t.Errorf("Test failed with error: %s", err)
+	}
+
+	expectedMemory := []int{-1, 4613515612218425347, 5, 2345108766317314046, 10, 10, -1, -1, -1, 5}
+	for i, v := range expectedMemory {
+		actual := relocatedMemory[i]
+		if actual != v {
+			t.Errorf("Expected relocated memory at index %d to be %d but it's %d", i, v, actual)
+		}
 	}
 }
