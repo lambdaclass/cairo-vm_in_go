4ec1c0c10640193b401bd8879ce2db49914a1f2a
gabrielbosio <gabrielbosio95@gmail.com>
2023-07-31 12:33:12 -0300

    Add segment relocation tests

--- pkg/vm/memory/segments_test.go
+++ pkg/vm/memory/segments_test.go
@@ -22,3 +22,35 @@ func TestRelocateOneSegment(t *testing.T) {
 		t.Errorf("Relocation tables are not the same")
 	}
 }
+
+func TestRelocateFiveSegments(t *testing.T) {
+	segments := memory.NewMemorySegmentManager()
+	segments.AddSegment()
+	segments.SegmentSizes = map[uint]uint{0: 3, 1: 3, 2: 56, 3: 78, 4: 8}
+	relocation_table, ok := segments.RelocateSegments()
+
+	if !ok {
+		t.Errorf("Memory segment manager doesn't have segment sizes initialized")
+	}
+
+	expected_table := []uint{1, 4, 7, 63, 141}
+	if !reflect.DeepEqual(expected_table, relocation_table) {
+		t.Errorf("Relocation tables are not the same")
+	}
+}
+
+func TestRelocateSegmentsWithHoles(t *testing.T) {
+	segments := memory.NewMemorySegmentManager()
+	segments.AddSegment()
+	segments.SegmentSizes = map[uint]uint{0: 3, 2: 3}
+	relocation_table, ok := segments.RelocateSegments()
+
+	if !ok {
+		t.Errorf("Memory segment manager doesn't have segment sizes initialized")
+	}
+
+	expected_table := []uint{1, 4, 4}
+	if !reflect.DeepEqual(expected_table, relocation_table) {
+		t.Errorf("Relocation tables are not the same")
+	}
+}
