c38d6ca457c1a68562b0fd0797efe232e275b013
Federica <fedemoletta@hotmail.com>
2023-07-31 16:01:51 -0300

    Make methods and variables private

--- pkg/runners/cairo_runner.go
+++ pkg/runners/cairo_runner.go
@@ -9,47 +9,47 @@ type CairoRunner struct {
 	Program       vm.Program
 	Vm            vm.VirtualMachine
 	ProgramBase   memory.Relocatable
-	ExecutionBase memory.Relocatable
-	InitialPc     memory.Relocatable
-	InitialAp     memory.Relocatable
-	InitialFp     memory.Relocatable
-	FinalPc       memory.Relocatable
-	MainOffset    uint
+	executionBase memory.Relocatable
+	initialPc     memory.Relocatable
+	initialAp     memory.Relocatable
+	initialFp     memory.Relocatable
+	finalPc       memory.Relocatable
+	mainOffset    uint
 }
 
 func NewCairoRunner(program vm.Program) *CairoRunner {
 	// TODO: Fetch main entrypoint offset from program identifiers
 	// Placeholder
 	main_offset := uint(0)
-	return &CairoRunner{Program: program, Vm: *vm.NewVirtualMachine(), MainOffset: main_offset}
+	return &CairoRunner{Program: program, Vm: *vm.NewVirtualMachine(), mainOffset: main_offset}
 
 }
 
 // Performs the initialization step, returns the end pointer (pc upon which execution should stop)
 func (r *CairoRunner) Initialize() (memory.Relocatable, error) {
-	r.InitializeSegments()
-	end, err := r.InitializeMainEntrypoint()
-	r.InitializeVM()
+	r.initializeSegments()
+	end, err := r.initializeMainEntrypoint()
+	r.initializeVM()
 	return end, err
 }
 
 // Creates program, execution and builtin segments
-func (r *CairoRunner) InitializeSegments() {
+func (r *CairoRunner) initializeSegments() {
 	// Program Segment
 	r.ProgramBase = r.Vm.Segments.AddSegment()
 	// Execution Segment
-	r.ExecutionBase = r.Vm.Segments.AddSegment()
+	r.executionBase = r.Vm.Segments.AddSegment()
 	// Initialize builtin segments
 }
 
 // Initializes the program segment & initial pc
 func (r *CairoRunner) initializeState(entrypoint uint, stack *[]memory.MaybeRelocatable) error {
-	r.InitialPc = r.ProgramBase
-	r.InitialPc.Offset += entrypoint
+	r.initialPc = r.ProgramBase
+	r.initialPc.Offset += entrypoint
 	// Load program data
 	_, err := r.Vm.Segments.LoadData(r.ProgramBase, &r.Program.Data)
 	if err != nil {
-		_, err = r.Vm.Segments.LoadData(r.ExecutionBase, stack)
+		_, err = r.Vm.Segments.LoadData(r.executionBase, stack)
 	}
 	// Mark data segment as accessed
 	return err
@@ -57,31 +57,31 @@ func (r *CairoRunner) initializeState(entrypoint uint, stack *[]memory.MaybeRelo
 
 // Initializes memory, initial register values & returns the end pointer (final pc) to run from a given pc offset
 // (entrypoint)
-func (r *CairoRunner) InitializeFunctionEntrypoint(entrypoint uint, stack *[]memory.MaybeRelocatable, return_fp memory.Relocatable) (memory.Relocatable, error) {
+func (r *CairoRunner) initializeFunctionEntrypoint(entrypoint uint, stack *[]memory.MaybeRelocatable, return_fp memory.Relocatable) (memory.Relocatable, error) {
 	end := r.Vm.Segments.AddSegment()
 	*stack = append(*stack, *memory.NewMaybeRelocatableRelocatable(end), *memory.NewMaybeRelocatableRelocatable(return_fp))
-	r.InitialFp = r.ExecutionBase
-	r.InitialFp.Offset += uint(len(*stack))
-	r.InitialAp = r.InitialFp
-	r.FinalPc = end
+	r.initialFp = r.executionBase
+	r.initialFp.Offset += uint(len(*stack))
+	r.initialAp = r.initialFp
+	r.finalPc = end
 	return end, r.initializeState(entrypoint, stack)
 }
 
 // Initializes memory, initial register values & returns the end pointer (final pc) to run from the main entrypoint
-func (r *CairoRunner) InitializeMainEntrypoint() (memory.Relocatable, error) {
+func (r *CairoRunner) initializeMainEntrypoint() (memory.Relocatable, error) {
 	// When running from main entrypoint, only up to 11 values will be written (9 builtin bases + end + return_fp)
 	stack := make([]memory.MaybeRelocatable, 0, 11)
 	// Append builtins initial stack to stack
 	// Handle proof-mode specific behaviour
 	return_fp := r.Vm.Segments.AddSegment()
-	return r.InitializeFunctionEntrypoint(r.MainOffset, &stack, return_fp)
+	return r.initializeFunctionEntrypoint(r.mainOffset, &stack, return_fp)
 }
 
 // Initializes the vm's run_context, adds builtin validation rules & validates memory
-func (r *CairoRunner) InitializeVM() {
-	r.Vm.RunContext.Ap = r.InitialAp
-	r.Vm.RunContext.Fp = r.InitialFp
-	r.Vm.RunContext.Pc = r.InitialPc
+func (r *CairoRunner) initializeVM() {
+	r.Vm.RunContext.Ap = r.initialAp
+	r.Vm.RunContext.Fp = r.initialFp
+	r.Vm.RunContext.Pc = r.initialPc
 	// Add validation rules
 	// Apply validation rules to memory
 }
