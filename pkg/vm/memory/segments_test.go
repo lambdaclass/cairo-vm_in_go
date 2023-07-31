8661157fb186fcfc8f74c47c161492a4f214a281
gabrielbosio <gabrielbosio95@gmail.com>
2023-07-31 12:40:12 -0300

    Normalize name convention

--- pkg/vm/memory/segments_test.go
+++ pkg/vm/memory/segments_test.go
@@ -11,14 +11,14 @@ func TestRelocateOneSegment(t *testing.T) {
 	segments := memory.NewMemorySegmentManager()
 	segments.AddSegment()
 	segments.SegmentSizes = map[uint]uint{0: 3}
-	relocation_table, ok := segments.RelocateSegments()
+	relocationTable, ok := segments.RelocateSegments()
 
 	if !ok {
 		t.Errorf("Memory segment manager doesn't have segment sizes initialized")
 	}
 
-	expected_table := []uint{1}
-	if !reflect.DeepEqual(expected_table, relocation_table) {
+	expectedTable := []uint{1}
+	if !reflect.DeepEqual(expectedTable, relocationTable) {
 		t.Errorf("Relocation tables are not the same")
 	}
 }
@@ -27,14 +27,14 @@ func TestRelocateFiveSegments(t *testing.T) {
 	segments := memory.NewMemorySegmentManager()
 	segments.AddSegment()
 	segments.SegmentSizes = map[uint]uint{0: 3, 1: 3, 2: 56, 3: 78, 4: 8}
-	relocation_table, ok := segments.RelocateSegments()
+	relocationTable, ok := segments.RelocateSegments()
 
 	if !ok {
 		t.Errorf("Memory segment manager doesn't have segment sizes initialized")
 	}
 
-	expected_table := []uint{1, 4, 7, 63, 141}
-	if !reflect.DeepEqual(expected_table, relocation_table) {
+	expectedTable := []uint{1, 4, 7, 63, 141}
+	if !reflect.DeepEqual(expectedTable, relocationTable) {
 		t.Errorf("Relocation tables are not the same")
 	}
 }
@@ -43,14 +43,14 @@ func TestRelocateSegmentsWithHoles(t *testing.T) {
 	segments := memory.NewMemorySegmentManager()
 	segments.AddSegment()
 	segments.SegmentSizes = map[uint]uint{0: 3, 2: 3}
-	relocation_table, ok := segments.RelocateSegments()
+	relocationTable, ok := segments.RelocateSegments()
 
 	if !ok {
 		t.Errorf("Memory segment manager doesn't have segment sizes initialized")
 	}
 
-	expected_table := []uint{1, 4, 4}
-	if !reflect.DeepEqual(expected_table, relocation_table) {
+	expectedTable := []uint{1, 4, 4}
+	if !reflect.DeepEqual(expectedTable, relocationTable) {
 		t.Errorf("Relocation tables are not the same")
 	}
 }
