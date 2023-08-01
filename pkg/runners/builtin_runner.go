258f1c66ddf304558666015a5fb64d8fe3c8d4b9
Federica <fedemoletta@hotmail.com>
2023-08-01 13:36:15 -0300

    Add commented expanded BuiltinRunner API

--- pkg/runners/builtin_runner.go
+++ pkg/runners/builtin_runner.go
@@ -17,4 +17,27 @@ type BuiltinRunner interface {
 	// Adds a validation rule to the memory
 	// Validation rules are applied when a value is inserted into the builtin's segment
 	AddValidationRule(*memory.Memory)
+	// TODO: Later additions -> Some of them could depend on a Default Implementation
+	// // Most of them depend on Layouts being implemented
+	// // Use cases:
+	// // I. PROOF_MODE
+	// // Returns the builtin's ratio, can be nil if the layout is dynamic
+	// Ratio() *uint // proof-mode end_run logic
+	// // Returns the builtin's allocated memory units
+	// GetAllocatedMemoryUnits(*vm.VirtualMachine) (uint, error) // proof-mode end_run logic
+	// // Returns the list of memory addresses used by the builtin
+	// GetMemoryAccesses(*memory.MemorySegmentManager) ([]memory.Relocatable, error) // proof-mode end_run logic
+	// GetUsedCells(*memory.MemorySegmentManager) (uint, error)                      // proof-mode end_run logic
+	// GetRangeCheckUsage(*memory.Memory) (*uint, *uint)                             // proof-mode end_run logic
+	// GetUsedPermRangeCheckLimits(*vm.VirtualMachine) (uint, error)                 // proof-mode end_run logic
+	// GetUsedDilutedCheckUnits(diluted_spacing uint, diluted_n_bits uint) uint      // proof-mode end_run logic
+	// GetUsedCellsAndAllocatedSizes(*vm.VirtualMachine) (uint, uint, error)         // proof-mode end_run logic + finalize_segments
+	// // II. SECURITY (secure-run flag cairo-run || verify-secure flag run_from_entrypoint)
+	// RunSecurityChecks(*vm.VirtualMachine) error // verify_secure_runner logic
+	// // Returns the base & stop_ptr, stop_ptr can be nil
+	// GetMemorySegmentAddresses() (memory.Relocatable, *memory.Relocatable) //verify_secure_runner logic
+	// // III. STARKNET-SPECIFIC
+	// GetUsedInstances(*memory.MemorySegmentManager) (uint, error) // get_execution_resources (starknet use case)
+	// // IV. GENERAL CASE (but not critical)
+	// FinalStack(*memory.MemorySegmentManager, memory.Relocatable) (memory.Relocatable, error) // read_return_values
 }
