c46e290ea0464172b132eb19b982d9d83de0723f
Federica <fedemoletta@hotmail.com>
2023-08-01 16:58:47 -0300

    Add BuiltinRunners field to VM

--- pkg/vm/vm_core.go
+++ pkg/vm/vm_core.go
@@ -1,11 +1,22 @@
 package vm
 
-import "github.com/lambdaclass/cairo-vm.go/pkg/vm/memory"
+import (
+	"github.com/lambdaclass/cairo-vm.go/pkg/builtins"
+	"github.com/lambdaclass/cairo-vm.go/pkg/vm/memory"
+)
 
 // VirtualMachine represents the Cairo VM.
 // Runs Cairo assembly and produces an execution trace.
 type VirtualMachine struct {
-	runContext  RunContext
-	currentStep uint
-	segments    memory.MemorySegmentManager
+	RunContext     RunContext
+	currentStep    uint
+	Segments       memory.MemorySegmentManager
+	BuiltinRunners []builtins.BuiltinRunner
+}
+
+func NewVirtualMachine() *VirtualMachine {
+	return &VirtualMachine{
+		Segments:       *memory.NewMemorySegmentManager(),
+		BuiltinRunners: make([]builtins.BuiltinRunner, 0, 9), // There will be at most 9 builtins
+	}
 }
