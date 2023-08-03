dae25e44eea9bc919cce284648db10fb4a466e0e
Federica <fedemoletta@hotmail.com>
2023-08-03 13:25:18 -0300

    Add test

--- pkg/vm/vm_test.go
+++ pkg/vm/vm_test.go
@@ -22,6 +22,20 @@ func TestUpdatePcRegularNoImm(t *testing.T) {
 
 }
 
+func TestUpdatePcRegularWithImm(t *testing.T) {
+	instruction := vm.Instruction{PcUpdate: vm.PcUpdateRegular, Op1Addr: vm.Op1SrcImm}
+	operands := vm.Operands{}
+	vm := vm.NewVirtualMachine()
+	err := vm.UpdatePc(&instruction, &operands)
+	if err != nil {
+		t.Errorf("UpdatePc failed with error: %s", err)
+	}
+	if !reflect.DeepEqual(vm.RunContext.Pc, memory.Relocatable{SegmentIndex: 0, Offset: 2}) {
+		t.Errorf("Wrong value after pc update")
+	}
+
+}
+
 // Things we are skipping for now:
 // - Initializing hint_executor and passing it to `cairo_run`
 // - cairo_run_config stuff
