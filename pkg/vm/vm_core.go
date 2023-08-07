bf205e79a296c46c3e78d36ec8a5e358d814869e
Milton <milton.scuderi@lambdaclass.com>
2023-08-07 12:50:59 -0300

    Move update registers function

--- pkg/vm/vm_core.go
+++ pkg/vm/vm_core.go
@@ -243,6 +243,20 @@ func (vm VirtualMachine) run_instrucion(instruction Instruction) {
 	fmt.Println("hello from instruction")
 }
 
+
+
+// Updates the values of the RunContext's registers according to the executed instruction
+func (vm *VirtualMachine) UpdateRegisters(instruction *Instruction, operands *Operands) error {
+	if err := vm.UpdateFp(instruction, operands); err != nil {
+		return err
+	}
+	if err := vm.UpdateAp(instruction, operands); err != nil {
+		return err
+	}
+	return vm.UpdatePc(instruction, operands)
+}
+
+
 // Updates the value of PC according to the executed instruction
 func (vm *VirtualMachine) UpdatePc(instruction *Instruction, operands *Operands) error {
 	switch instruction.PcUpdate {
@@ -283,17 +297,6 @@ func (vm *VirtualMachine) UpdatePc(instruction *Instruction, operands *Operands)
 
 	}
 	return nil
-}
-
-// Updates the values of the RunContext's registers according to the executed instruction
-func (vm *VirtualMachine) UpdateRegisters(instruction *Instruction, operands *Operands) error {
-	if err := vm.UpdateFp(instruction, operands); err != nil {
-		return err
-	}
-	if err := vm.UpdateAp(instruction, operands); err != nil {
-		return err
-	}
-	return vm.UpdatePc(instruction, operands)
 }
 
 // Updates the value of AP according to the executed instruction
