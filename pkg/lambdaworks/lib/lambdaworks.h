dd4f054f683f71b4e89b2f7fd069b753e788c6bc
gabrielbosio <gabrielbosio95@gmail.com>
2023-08-08 17:38:49 -0300

    Add le bytes wrapper

--- pkg/lambdaworks/lib/lambdaworks.h
+++ pkg/lambdaworks/lib/lambdaworks.h
@@ -1,18 +1,25 @@
+#include <_types/_uint8_t.h>
 #include <stdint.h>
 
 typedef uint64_t limb_t;
 
-/* A 256 bit prime field element (felt), represented as four limbs (integers). */
+/* A 256 bit prime field element (felt), represented as four limbs (integers).
+ */
 typedef limb_t felt_t[4];
 
 /* Gets a felt_t representing the "value" number, in montgomery format. */
 void from(felt_t result, uint64_t value);
 
-/*Gets a felt_t representing the "value" hexadecimal string, in montgomery format. */
-void from_hex(felt_t result,  char *value);
+/*Gets a felt_t representing the "value" hexadecimal string, in montgomery
+ * format. */
+void from_hex(felt_t result, char *value);
 
-/*Gets a felt_t representing the "value" decimal string, in montgomery format. */
+/*Gets a felt_t representing the "value" decimal string, in montgomery format.
+ */
 void from_dec_str(felt_t result, char *value);
+
+/* Converts a felt_t to bytes in little-endian representation. */
+void to_le_bytes(uint8_t result[32], felt_t value);
 
 /* Gets a felt_t representing 0 */
 void zero(felt_t result);
