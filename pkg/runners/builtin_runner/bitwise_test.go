e1f2713352cb322a4cf668d0e97376a5a3350fa0
Milton <milton.scuderi@lambdaclass.com>
2023-08-10 12:04:54 -0300

    Add test

---
+++ pkg/runners/builtin_runner/bitwise_test.go
@@ -0,0 +1,42 @@
+package builtinrunner
+
+import (
+	"testing"
+
+	"github.com/lambdaclass/cairo-vm.go/pkg/lambdaworks"
+	"github.com/lambdaclass/cairo-vm.go/pkg/vm/memory"
+)
+
+func TestDeduceMemoryCellBitwiseForPresetMemoryValidAnd(t *testing.T) {
+	mem := memory.NewMemory()
+	rel1 := memory.NewRelocatable(0, 5)
+	rel2 := memory.NewRelocatable(0, 6)
+	rel3 := memory.NewRelocatable(0, 7)
+
+	m1 := memory.NewMaybeRelocatableFelt(lambdaworks.FeltFromUint64(10))
+	m2 := memory.NewMaybeRelocatableFelt(lambdaworks.FeltFromUint64(12))
+	m3 := memory.NewMaybeRelocatableFelt(lambdaworks.FeltFromUint64(0))
+
+	mem.Insert(rel1, m1)
+	mem.Insert(rel2, m2)
+	mem.Insert(rel3, m3)
+
+	var ratio uint = 256
+
+	builtin_instance := BitwiseInstanceDef{Ratio: &ratio, TotalNBits: 251}
+	builtin := NewBitwiseBuiltinRunner(builtin_instance, true)
+
+	address := memory.NewRelocatable(0, 8)
+
+	result, err := builtin.DeduceMemoryCell(address, mem)
+	expected := memory.NewMaybeRelocatableFelt(lambdaworks.FeltFromUint64(6))
+
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	if result != expected {
+		t.Errorf("TestDeduceMemoryCellBitwiseForPresetMemoryValidAnd Failed, expected %v, got %v", expected, result)
+	}
+
+}
