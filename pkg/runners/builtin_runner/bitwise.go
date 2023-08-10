4001efc97e21c12ec740acd0d0aba5e0806a3f05
Milton <milton.scuderi@lambdaclass.com>
2023-08-10 12:22:55 -0300

    Add more tests, delete debug info

--- pkg/runners/builtin_runner/bitwise.go
+++ pkg/runners/builtin_runner/bitwise.go
@@ -53,26 +53,22 @@ func (b *BitwiseBuiltinRunner) InitialStack() []memory.MaybeRelocatable {
 func (b *BitwiseBuiltinRunner) DeduceMemoryCell(address memory.Relocatable, segments *memory.Memory) (*memory.MaybeRelocatable, error) {
 	index := address.Offset % b.CellsPerInstance
 	if index < 1 {
-		fmt.Println("index < 1")
 		return nil, nil
 	}
 
 	x_addr := memory.NewRelocatable(address.SegmentIndex, address.Offset-index)
 	y_addr, err := (x_addr.AddUint(1))
 	if err != nil {
-		fmt.Println("could not fetch y_addr")
 		return nil, err
 	}
 	num_x, err := segments.Get(x_addr)
 	if err != nil {
-		fmt.Println("could not fetch num x")
 
 		return nil, err
 	}
 
 	num_y, err := segments.Get(y_addr)
 	if err != nil {
-		fmt.Println("could not fetch num y")
 
 		return nil, err
 	}
@@ -81,7 +77,6 @@ func (b *BitwiseBuiltinRunner) DeduceMemoryCell(address memory.Relocatable, segm
 	num_y_felt, y_is_felt := num_y.GetFelt()
 
 	if x_is_felt && y_is_felt {
-		fmt.Println("enter the critical if")
 		// TODO: check if this conversion if valid
 		if uint64(num_x_felt.Bits()) > uint64(b.BitwiseBuiltin.TotalNBits) {
 			return nil, errors.New("Expected Intenger x to be smaller than 2^(total_n_bits)")
--- pkg/runners/builtin_runner/bitwise_test.go
+++ pkg/runners/builtin_runner/bitwise_test.go
@@ -109,3 +109,66 @@ func TestDeduceMemoryCellBitwiseForPresetMemoryValidOr(t *testing.T) {
 	}
 
 }
+
+func TestDeduceMemoryCellBitwiseForPresetMemoryIncorrectOffset(t *testing.T) {
+	mem := memory.NewMemorySegmentManager()
+	mem.AddSegment()
+	rel1 := memory.NewRelocatable(0, 3)
+	rel2 := memory.NewRelocatable(0, 4)
+	rel3 := memory.NewRelocatable(0, 5)
+
+	m1 := memory.NewMaybeRelocatableFelt(lambdaworks.FeltFromUint64(10))
+	m2 := memory.NewMaybeRelocatableFelt(lambdaworks.FeltFromUint64(12))
+	m3 := memory.NewMaybeRelocatableFelt(lambdaworks.FeltFromUint64(0))
+
+	mem.Memory.Insert(rel1, m1)
+	mem.Memory.Insert(rel2, m2)
+	mem.Memory.Insert(rel3, m3)
+
+	var ratio uint = 256
+	builtin_instance := BitwiseInstanceDef{Ratio: &ratio, TotalNBits: 251}
+	builtin := NewBitwiseBuiltinRunner(builtin_instance, true)
+
+	address := memory.NewRelocatable(0, 5)
+
+	result, err := builtin.DeduceMemoryCell(address, &mem.Memory)
+
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	if result != nil {
+		t.Errorf("TestDeduceMemoryCellBitwiseForPresetMemoryValidAnd Failed, expected nil, got %v", result)
+	}
+
+}
+
+func TestDeduceMemoryCellBitwiseForPresetMemoryNoValuesToOperate(t *testing.T) {
+	mem := memory.NewMemorySegmentManager()
+	mem.AddSegment()
+	rel1 := memory.NewRelocatable(0, 5)
+	rel2 := memory.NewRelocatable(0, 7)
+
+	m1 := memory.NewMaybeRelocatableFelt(lambdaworks.FeltFromUint64(10))
+	m2 := memory.NewMaybeRelocatableFelt(lambdaworks.FeltFromUint64(0))
+
+	mem.Memory.Insert(rel1, m1)
+	mem.Memory.Insert(rel2, m2)
+
+	var ratio uint = 256
+	builtin_instance := BitwiseInstanceDef{Ratio: &ratio, TotalNBits: 251}
+	builtin := NewBitwiseBuiltinRunner(builtin_instance, true)
+
+	address := memory.NewRelocatable(0, 5)
+
+	result, err := builtin.DeduceMemoryCell(address, &mem.Memory)
+
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	if result != nil {
+		t.Errorf("TestDeduceMemoryCellBitwiseForPresetMemoryValidAnd Failed, expected nil, got %v", result)
+	}
+
+}
