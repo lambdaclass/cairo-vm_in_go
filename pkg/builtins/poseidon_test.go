ea62eaf15d134018dbe2bf024901aa536adf3ff1
Federica <fedemoletta@hotmail.com>
2023-08-10 18:21:02 -0300

    Add more tests

--- pkg/builtins/poseidon_test.go
+++ pkg/builtins/poseidon_test.go
@@ -5,8 +5,60 @@ import (
 	"testing"
 
 	"github.com/lambdaclass/cairo-vm.go/pkg/builtins"
+	"github.com/lambdaclass/cairo-vm.go/pkg/lambdaworks"
+	"github.com/lambdaclass/cairo-vm.go/pkg/vm"
 	"github.com/lambdaclass/cairo-vm.go/pkg/vm/memory"
 )
+
+func TestDeduceMemoryCellValid(t *testing.T) {
+	poseidon := builtins.NewPoseidonBuiltinRunner(true)
+	vmachine := vm.NewVirtualMachine()
+	vmachine.BuiltinRunners = append(vmachine.BuiltinRunners, poseidon)
+
+	// Insert input cells into memory
+	vmachine.Segments.AddSegment()
+	vmachine.Segments.Memory.Insert(
+		memory.NewRelocatable(0, 0),
+		memory.NewMaybeRelocatableFelt(lambdaworks.FeltFromHex("0x268c44203f1c763bca21beb5aec78b9063cdcdd0fdf6b598bb8e1e8f2b6253f")),
+	)
+	vmachine.Segments.Memory.Insert(
+		memory.NewRelocatable(0, 1),
+		memory.NewMaybeRelocatableFelt(lambdaworks.FeltFromHex("0x2b85c9f686f5d3036db55b2ca58a763a3065bc1bc8efbe0e70f3a7171f6cad3")),
+	)
+	vmachine.Segments.Memory.Insert(
+		memory.NewRelocatable(0, 2),
+		memory.NewMaybeRelocatableFelt(lambdaworks.FeltFromHex("0x61df3789eef0e1ee0dbe010582a00dd099191e6395dfb976e7be3be2fa9d54b")),
+	)
+
+	addr := memory.NewRelocatable(0, 5)
+	expected_last_output_cell := memory.NewMaybeRelocatableFelt(lambdaworks.FeltFromHex("0x749d4d0ddf41548e039f183b745a08b80fad54e9ac389021148350bdda70a92"))
+
+	val, err := vmachine.DeduceMemoryCell(addr)
+	if !reflect.DeepEqual(val, expected_last_output_cell) || err != nil {
+		t.Errorf("Wrong values returned by DeduceMemoryCell")
+	}
+}
+
+func TestDeduceMemoryCellNoInputCells(t *testing.T) {
+	poseidon := builtins.NewPoseidonBuiltinRunner(true)
+	vmachine := vm.NewVirtualMachine()
+	vmachine.BuiltinRunners = append(vmachine.BuiltinRunners, poseidon)
+	addr := memory.NewRelocatable(0, 5)
+	val, err := vmachine.DeduceMemoryCell(addr)
+	if val != nil || err == nil {
+		t.Errorf("Wrong values returned by DeduceMemoryCell")
+	}
+}
+func TestDeduceMemoryCellInputCell(t *testing.T) {
+	poseidon := builtins.NewPoseidonBuiltinRunner(true)
+	vmachine := vm.NewVirtualMachine()
+	vmachine.BuiltinRunners = append(vmachine.BuiltinRunners, poseidon)
+	addr := memory.NewRelocatable(0, 1)
+	val, err := vmachine.DeduceMemoryCell(addr)
+	if val != nil || err != nil {
+		t.Errorf("Wrong values returned by DeduceMemoryCell")
+	}
+}
 
 func TestInitializeSegments(t *testing.T) {
 	mem_manager := memory.NewMemorySegmentManager()
