37b706f1caae73cb868a689797c783bbe5180b49
Federica <fedemoletta@hotmail.com>
2023-08-10 19:03:35 -0300

    fix/add header comments

--- pkg/starknet_crypto/lib/starknet_crypto.h
+++ pkg/starknet_crypto/lib/starknet_crypto.h
@@ -2,9 +2,8 @@
 
 typedef uint8_t byte_t;
 
-/* A 256 bit prime field element (felt), represented as four limbs (integers).
- */
+// A 252 bit prime field element (felt), represented as an array of 32 bytes.
 typedef byte_t felt_t[32];
 
-
+// Computes the poseidon hash permutation over a state of three felts
 void poseidon_permute(felt_t, felt_t, felt_t);
