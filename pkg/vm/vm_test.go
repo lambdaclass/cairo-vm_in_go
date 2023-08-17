5a188cfeffd9914621586b1f3335599c8d381b42
Matthew Coburn <mattcoburn24@gmail.com>
2023-08-16 23:01:01 -0700

    add Test for GetRelocateTrace without relocating trace
    added TestGetRelocateTraceWithoutRelocatingTrace

--- pkg/vm/vm_test.go
+++ pkg/vm/vm_test.go
@@ -519,6 +519,14 @@ func TestDeduceMemoryCellNoBuiltins(t *testing.T) {
 	}
 }
 
+func TestRelocateTraceWithoutRelocatingTrace(t *testing.T) {
+	virtualMachine := vm.NewVirtualMachine()
+	_, err := virtualMachine.GetRelocatedTrace()
+	if err == nil {
+		t.Fatalf("GetRelocatedTrace should have failed")
+	}
+}
+
 func TestRelocateTraceOneEntry(t *testing.T) {
 	virtualMachine := vm.NewVirtualMachine()
 	buildTestProgramMemory(virtualMachine)
