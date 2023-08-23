e776a1cecfff787801f5738c55b46740d5e31c9a
Antonio Calvín García <garcia.calvin.toni@gmail.com>
2023-08-23 22:39:04 +0200

    Range check builtin (#52)
    * WIP: Implementing range_check builtin
    - Adding constant declarations
    - Adding memory error
    - Implementing first draft of builtin
    
    * Implementing range_check builtin
    - Add Bits function on lambdaworks API
    - Implements builtin_runner interface on range_check builtin.
    
    * Testing range_check builtin
    - Renamed to Uppercase some attributes
    
    * Fixing felt inicialization
    
    * Fixing felt inicialization
    
    * Update pkg/runners/builtin_runner/range_check.go
    
    Co-authored-by: fmoletta <99273364+fmoletta@users.noreply.github.com>
    
    * Creating class method One and Zero
    
    * Deleting not used fields in RangeCheckBuiltinRunner
    
    * Update pkg/runners/builtin_runner/range_check.go
    
    Co-authored-by: fmoletta <99273364+fmoletta@users.noreply.github.com>
    
    * Update pkg/runners/builtin_runner/range_check.go
    
    Co-authored-by: fmoletta <99273364+fmoletta@users.noreply.github.com>
    
    * Defininf builtin.base as Relocatable
    
    * Using lambdaworks shl method
    
    * Deleting unnecessary method (for now)
    
    * Improve error handeling
    
    * Added check range documentation
    
    * Update pkg/lambdaworks/lambdaworks_test.go
    
    Co-authored-by: Gabriel Bosio <38794644+gabrielbosio@users.noreply.github.com>
    
    * Update pkg/lambdaworks/lambdaworks_test.go
    
    Co-authored-by: Gabriel Bosio <38794644+gabrielbosio@users.noreply.github.com>
    
    * Typos
    
    * Fixing some merge conflicts
    
    * Fixing TestValidateExistingMemoryForRangeCheckOutsideBounds
    
    * Remove unused lambdaworks methods
    
    * Integration tests for check range builtin
    
    * Review PR comments
    
    * Make Memory fields private again
    
    * Fixing tests
    
    * Fixing commented test
    
    * Adding more documentation about range check
    
    Co-authored-by: Mauro Toscano <12560266+MauroToscano@users.noreply.github.com>
    
    * Fix attribute access
    
    * Merge branch 'main' of github.com:lambdaclass/cairo-vm.go into range-check-builtin
    
    * Fix errors
    
    * Apply suggestions from code review
    
    Co-authored-by: Gabriel Bosio <38794644+gabrielbosio@users.noreply.github.com>
    
    * Remove unused modules
    
    * Error capitalization
    
    * Address comment
    
    * Update README.md
    
    Co-authored-by: fmoletta <99273364+fmoletta@users.noreply.github.com>
    
    * Update pkg/builtins/range_check.go
    
    Co-authored-by: fmoletta <99273364+fmoletta@users.noreply.github.com>
    
    * Address comment
    
    * Better explanation
    
    * Improve error message
    
    ---------
    
    Co-authored-by: fmoletta <99273364+fmoletta@users.noreply.github.com>
    Co-authored-by: Gabriel Bosio <38794644+gabrielbosio@users.noreply.github.com>
    Co-authored-by: Mauro Toscano <12560266+MauroToscano@users.noreply.github.com>
    Co-authored-by: Javier Chatruc <jrchatruc@gmail.com>
    Co-authored-by: Javier Rodríguez Chatruc <49622509+jrchatruc@users.noreply.github.com>

---
+++ pkg/builtins/range_check.go
@@ -0,0 +1,82 @@
+package builtins
+
+import (
+	"math"
+
+	"github.com/lambdaclass/cairo-vm.go/pkg/lambdaworks"
+	"github.com/lambdaclass/cairo-vm.go/pkg/vm/memory"
+	"github.com/pkg/errors"
+)
+
+const CHECK_RANGE_BUILTIN_NAME = "range_check"
+const INNER_RC_BOUND_SHIFT = 16
+const INNER_RC_BOUND_MASK = math.MaxUint16
+const CELLS_PER_RANGE_CHECK = 1
+
+const N_PARTS = 8
+
+func RangeCheckError(err error) error {
+	return errors.Wrapf(err, "Range check error")
+}
+
+func OutsideBoundsError(felt lambdaworks.Felt) error {
+	return RangeCheckError(errors.Errorf("Value %d is out of bounds [0, 2^128]", felt))
+}
+
+func NotAFeltError(addr memory.Relocatable, val memory.MaybeRelocatable) error {
+	rel, _ := val.GetRelocatable()
+	return RangeCheckError(errors.Errorf("Value %d found in %d is not a field element", rel, addr))
+}
+
+type RangeCheckBuiltinRunner struct {
+	base     memory.Relocatable
+	included bool
+}
+
+func NewRangeCheckBuiltinRunner(included bool) *RangeCheckBuiltinRunner {
+	return &RangeCheckBuiltinRunner{
+		included: included,
+	}
+}
+
+func (r *RangeCheckBuiltinRunner) Base() memory.Relocatable {
+	return r.base
+}
+
+func (r *RangeCheckBuiltinRunner) Name() string {
+	return CHECK_RANGE_BUILTIN_NAME
+}
+
+func (r *RangeCheckBuiltinRunner) InitializeSegments(segments *memory.MemorySegmentManager) {
+	r.base = segments.AddSegment()
+}
+
+func (r *RangeCheckBuiltinRunner) InitialStack() []memory.MaybeRelocatable {
+	if r.included {
+		return []memory.MaybeRelocatable{*memory.NewMaybeRelocatableRelocatable(r.base)}
+	}
+	return []memory.MaybeRelocatable{}
+}
+
+func (r *RangeCheckBuiltinRunner) DeduceMemoryCell(addr memory.Relocatable, mem *memory.Memory) (*memory.MaybeRelocatable, error) {
+	return nil, nil
+}
+
+func ValidationRule(mem *memory.Memory, address memory.Relocatable) ([]memory.Relocatable, error) {
+	res_val, err := mem.Get(address)
+	if err != nil {
+		return nil, err
+	}
+	felt, is_felt := res_val.GetFelt()
+	if !is_felt {
+		return nil, NotAFeltError(address, *res_val)
+	}
+	if felt.Bits() <= N_PARTS*INNER_RC_BOUND_SHIFT {
+		return []memory.Relocatable{address}, nil
+	}
+	return nil, OutsideBoundsError(felt)
+}
+
+func (r *RangeCheckBuiltinRunner) AddValidationRule(mem *memory.Memory) {
+	mem.AddValidationRule(uint(r.base.SegmentIndex), ValidationRule)
+}
