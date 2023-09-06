f499b698b8d25fea337f7a0379beb41f38a263b5
Federica <fedemoletta@hotmail.com>
2023-09-06 17:20:29 -0300

    Add comments

--- pkg/hints/hint_utils.go
+++ pkg/hints/hint_utils.go
@@ -38,6 +38,18 @@ func ParseHintReference(reference parser.Reference) HintReference {
 	value_string, has_prefix := string.CutPrefix(value_string, '[')
 	value_string, has_suffix := string.CutSuffix(value_string, ']')
 	var dereference = has_prefix && has_suffix
+	// The string should always follow with cast(...), so lets trim this part
+	value_string = string.CutPrefix("cast(")
+	value_string = string.CutSuffix(")")
+	// TODO, use the second return to reject invalid strings
+	// Now we should consider the possible cases
+	//I. Inmediate value (number, type)
+	//II. Reference (reference, type)
+	// References can be made up of up to 2 offset values
+	// The first offset value will always be register + offset1
+	// and it can be dereferenced ([reg + off]) or not (ref + off)
+	// The second offset can be a register and offset (reg + off), or just an an offset(off).
+	// It can be dereferenced or not
 
 	return HintReference{ap_tracking_data: reference.ApTrackingData}
 }
