15793dde544f99fff84c4441b087757e0aff557e
Federica <fedemoletta@hotmail.com>
2023-09-06 17:00:28 -0300

    Start parsing by hand

--- pkg/hints/hint_utils.go
+++ pkg/hints/hint_utils.go
@@ -1,7 +1,10 @@
 package hint_utils
 
 import (
+	"string"
+
 	. "github.com/lambdaclass/cairo-vm.go/pkg/lambdaworks"
+	"github.com/lambdaclass/cairo-vm.go/pkg/parser"
 	"github.com/lambdaclass/cairo-vm.go/pkg/vm"
 )
 
@@ -9,12 +12,7 @@ type HintReference struct {
 	offset1          OffsetValue
 	offset2          OffsetValue
 	dereference      bool
-	ap_tracking_data ApTrackingData
-}
-
-type ApTrackingData struct {
-	group  uint
-	offset uint
+	ap_tracking_data parser.ApTrackingData
 }
 
 type OffsetValue struct {
@@ -32,3 +30,14 @@ const (
 	Value     offsetValueType = 1
 	Reference offsetValueType = 2
 )
+
+func ParseHintReference(reference parser.Reference) HintReference {
+	var value_string = reference.Value
+	// Trim outer brackets if dereference
+	// example [cast(reg + offset1, type)] -> cast(reg + offset1, type), dereference = true
+	value_string, has_prefix := string.CutPrefix(value_string, '[')
+	value_string, has_suffix := string.CutSuffix(value_string, ']')
+	var dereference = has_prefix && has_suffix
+
+	return HintReference{ap_tracking_data: reference.ApTrackingData}
+}
