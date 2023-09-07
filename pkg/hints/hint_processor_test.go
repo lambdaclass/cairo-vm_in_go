c20d0f7acc664819db93117494402ef14f37df45
Federica <fedemoletta@hotmail.com>
2023-09-07 17:47:54 -0300

    Add tests

---
+++ pkg/hints/hint_processor_test.go
@@ -0,0 +1,68 @@
+package hints_test
+
+import (
+	"reflect"
+	"testing"
+
+	. "github.com/lambdaclass/cairo-vm.go/pkg/hints"
+	. "github.com/lambdaclass/cairo-vm.go/pkg/hints/hint_utils"
+	"github.com/lambdaclass/cairo-vm.go/pkg/parser"
+)
+
+func TestCompileHintEmpty(t *testing.T) {
+	hintProcessor := &CairoVmHintProcessor{}
+	hintParams := &parser.HintParams{}
+	referenceManager := &parser.ReferenceManager{}
+	expectedData := HintData{}
+	data, err := hintProcessor.CompileHint(hintParams, referenceManager)
+	if err != nil {
+		t.Errorf("Error in test: %s", err)
+	}
+	if reflect.DeepEqual(data.(HintData), expectedData) {
+		t.Errorf("Wrong hint data, %+v", data)
+	}
+}
+
+func TestCompileHintHappyPath(t *testing.T) {
+	hintProcessor := &CairoVmHintProcessor{}
+	hintParams := &parser.HintParams{
+		Code: "ids.a = ids.b",
+		FlowTrackingData: parser.FlowTrackingData{
+			APTracking: parser.ApTrackingData{Group: 1, Offset: 2},
+		},
+		ReferenceIds: map[string]uint{"a": 0, "b": 1},
+	}
+	referenceManager := &parser.ReferenceManager{
+		References: []parser.Reference{
+			{
+				Value: "cast(ap + (-2))",
+			},
+			{
+				Value: "cast(ap + (-1))",
+			},
+		},
+	}
+	expectedData := HintData{
+		Ids: map[string]HintReference{
+			"a": {
+				Offset1: OffsetValue{
+					Value: -2,
+				},
+			},
+			"b": {
+				Offset1: OffsetValue{
+					Value: -1,
+				},
+			},
+		},
+		Code:       "ids.a = ids.b",
+		ApTracking: parser.ApTrackingData{Group: 1, Offset: 2},
+	}
+	data, err := hintProcessor.CompileHint(hintParams, referenceManager)
+	if err != nil {
+		t.Errorf("Error in test: %s", err)
+	}
+	if reflect.DeepEqual(data.(HintData), expectedData) {
+		t.Errorf("Wrong hint data, %+v", data)
+	}
+}
