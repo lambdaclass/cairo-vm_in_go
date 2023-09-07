4a46f6f2b7909c01ff18877e80b8b9ae8088deb5
juan.mv <juan@lambdaclass.com>
2023-09-07 13:06:00 -0300

    Rename add validation rule

--- pkg/builtins/range_check.go
+++ pkg/builtins/range_check.go
@@ -62,7 +62,7 @@ func (r *RangeCheckBuiltinRunner) DeduceMemoryCell(addr memory.Relocatable, mem 
 	return nil, nil
 }
 
-func ValRule(mem *memory.Memory, address memory.Relocatable) ([]memory.Relocatable, error) {
+func RangeCheckValidationRule(mem *memory.Memory, address memory.Relocatable) ([]memory.Relocatable, error) {
 	res_val, err := mem.Get(address)
 	if err != nil {
 		return nil, err
@@ -78,5 +78,5 @@ func ValRule(mem *memory.Memory, address memory.Relocatable) ([]memory.Relocatab
 }
 
 func (r *RangeCheckBuiltinRunner) AddValidationRule(mem *memory.Memory) {
-	mem.AddValidationRule(uint(r.base.SegmentIndex), ValRule)
+	mem.AddValidationRule(uint(r.base.SegmentIndex), RangeCheckValidationRule)
 }
