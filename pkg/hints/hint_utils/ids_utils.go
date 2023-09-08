d0d04ca894f9d9997597d8ac601ff5c1313a075b
Federica <fedemoletta@hotmail.com>
2023-09-08 13:32:19 -0300

    Progress

--- pkg/hints/hint_utils/ids_utils.go
+++ pkg/hints/hint_utils/ids_utils.go
@@ -13,8 +13,9 @@ func GetAddressFromReference(reference *HintReference, apTracking parser.ApTrack
 
 }
 
+// Returns the value of the offset, or nil of it can't be computed
 // Asumes offsetValue is of type Reference
-func getOffsetValueReference(offsetValue OffsetValue, refApTracking parser.ApTrackingData, hintApTracking parser.ApTrackingData, vm *VirtualMachine) {
+func getOffsetValueReference(offsetValue OffsetValue, refApTracking parser.ApTrackingData, hintApTracking parser.ApTrackingData, vm *VirtualMachine) *MaybeRelocatable {
 	var base_addr Relocatable
 	ok := true
 	switch offsetValue.Register {
@@ -24,8 +25,18 @@ func getOffsetValueReference(offsetValue OffsetValue, refApTracking parser.ApTra
 		base_addr, ok = applyApTrackingCorrection(vm.RunContext.Ap, refApTracking, hintApTracking)
 	}
 	if ok {
-
+		base_addr, err := base_addr.AddInt(offsetValue.Value)
+		if err == nil {
+			if offsetValue.Dereference {
+				// val will be nil if err is not nil, so we can ignore it
+				val, _ := vm.Segments.Memory.Get(base_addr)
+				return val
+			} else {
+				return NewMaybeRelocatableRelocatable(base_addr)
+			}
+		}
 	}
+	return nil
 }
 
 func applyApTrackingCorrection(addr Relocatable, refApTracking parser.ApTrackingData, hintApTracking parser.ApTrackingData) (Relocatable, bool) {
