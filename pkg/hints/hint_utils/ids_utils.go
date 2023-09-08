2c49b25ccebe55dc8a563e1b811a3abd1403f5f3
Federica <fedemoletta@hotmail.com>
2023-09-08 13:40:53 -0300

    Conclude GetAddressFromReference

--- pkg/hints/hint_utils/ids_utils.go
+++ pkg/hints/hint_utils/ids_utils.go
@@ -6,10 +6,33 @@ import (
 	. "github.com/lambdaclass/cairo-vm.go/pkg/vm/memory"
 )
 
+// Returns the addr indicated by the reference
 func GetAddressFromReference(reference *HintReference, apTracking parser.ApTrackingData, vm *VirtualMachine) (Relocatable, bool) {
 	if reference.Offset1.ValueType != Reference {
-		return nil
+		return Relocatable{}, false
 	}
+	offset1 := getOffsetValueReference(reference.Offset1, reference.ApTrackingData, apTracking, vm)
+	if offset1 != nil {
+		offset1_rel, is_rel := offset1.GetRelocatable()
+		if is_rel {
+			switch reference.Offset2.ValueType {
+			case Reference:
+				offset2 := getOffsetValueReference(reference.Offset2, reference.ApTrackingData, apTracking, vm)
+				if offset2 != nil {
+					res, err := offset1_rel.AddMaybeRelocatable(*offset2)
+					if err == nil {
+						return res, true
+					}
+				}
+			case Value:
+				res, err := offset1_rel.AddInt(reference.Offset2.Value)
+				if err == nil {
+					return res, true
+				}
+			}
+		}
+	}
+	return Relocatable{}, false
 
 }
 
