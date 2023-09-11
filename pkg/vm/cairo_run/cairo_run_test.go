7643b2627cc03bd29ab788a80f9e5255024d90cf
Federica <fedemoletta@hotmail.com>
2023-09-11 18:53:27 -0300

    Implement ASSERT_NN hint

--- pkg/vm/cairo_run/cairo_run_test.go
+++ pkg/vm/cairo_run/cairo_run_test.go
@@ -79,3 +79,10 @@ func TestPedersenAndBitwiseBuiltinsWithAllocLocals(t *testing.T) {
 		t.Errorf("Program execution failed with error: %s", err)
 	}
 }
+
+func TestAssertNNHint(t *testing.T) {
+	_, err := cairo_run.CairoRun("../../../cairo_programs/assert_nn.json", "small", false)
+	if err != nil {
+		t.Errorf("Program execution failed with error: %s", err)
+	}
+}
