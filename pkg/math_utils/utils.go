222571b7f61c616594ba15c4b83f9fd312b8bd21
juan.mv <juan@lambdaclass.com>
2023-09-12 12:52:10 -0300

    make fmt

--- pkg/math_utils/utils.go
+++ pkg/math_utils/utils.go
@@ -1,26 +1,26 @@
 package math_utils
 
 import (
-    "math/big"
-    "github.com/pkg/errors"
+	"github.com/pkg/errors"
+	"math/big"
 )
 
 // Finds a nonnegative integer x < p such that (m * x) % p == n.
 func DivMod(n *big.Int, m *big.Int, p *big.Int) (*big.Int, error) {
-    if m.BitLen() == 0 {
-        return nil, errors.Errorf("m in div_mod(n, m, p) can't be zero")
-    }
-    var inv_m, res *big.Int
-    inv_m.ModInverse(m, p)
-    res.Mul(inv_m, n)
-    res.Mod(res, p)
-    return res, nil
+	if m.BitLen() == 0 {
+		return nil, errors.Errorf("m in div_mod(n, m, p) can't be zero")
+	}
+	var inv_m, res *big.Int
+	inv_m.ModInverse(m, p)
+	res.Mul(inv_m, n)
+	res.Mod(res, p)
+	return res, nil
 }
 
 func ISqrt(x *big.Int) (*big.Int, error) {
-    if x.Sign() == -1 {
-        errors.Errorf("Expected x: %s to be non-negative", x)
-    }
-    var res *big.Int
-    return res.Sqrt(x), nil
+	if x.Sign() == -1 {
+		errors.Errorf("Expected x: %s to be non-negative", x)
+	}
+	var res *big.Int
+	return res.Sqrt(x), nil
 }
--- pkg/math_utils/utils_test.go
+++ pkg/math_utils/utils_test.go
@@ -1,58 +1,57 @@
 package math_utils_test
 
-import(
-    "testing"
-    "math/big"
-    . "github.com/lambdaclass/cairo-vm.go/pkg/math_utils"
+import (
+	. "github.com/lambdaclass/cairo-vm.go/pkg/math_utils"
+	"math/big"
+	"testing"
 )
 
 func div_mod_ok(t *testing.T) {
-    var a, b, prime, expected *big.Int
-    a.SetString("11260647941622813594563746375280766662237311019551239924981511729608487775604310196863705127454617186486639011517352066501847110680463498585797912894788", 10)
-    b.SetString("4020711254448367604954374443741161860304516084891705811279711044808359405970", 10)
-    prime.SetString("800000000000011000000000000000000000000000000000000000000000001", 16)
-    expected.SetString("2904750555256547440469454488220756360634457312540595732507835416669695939476", 10)
+	var a, b, prime, expected *big.Int
+	a.SetString("11260647941622813594563746375280766662237311019551239924981511729608487775604310196863705127454617186486639011517352066501847110680463498585797912894788", 10)
+	b.SetString("4020711254448367604954374443741161860304516084891705811279711044808359405970", 10)
+	prime.SetString("800000000000011000000000000000000000000000000000000000000000001", 16)
+	expected.SetString("2904750555256547440469454488220756360634457312540595732507835416669695939476", 10)
 
-    num, err := DivMod(a, b, prime)
-    if err != nil {
-        t.Errorf("DivMod failed with error: %s", err)
-    }
-    if num.Cmp(expected) != 0 {
-        t.Errorf("Expected result: %s to be equal to %s", num, expected)
-    }
+	num, err := DivMod(a, b, prime)
+	if err != nil {
+		t.Errorf("DivMod failed with error: %s", err)
+	}
+	if num.Cmp(expected) != 0 {
+		t.Errorf("Expected result: %s to be equal to %s", num, expected)
+	}
 }
 
 func div_mod_fail(t *testing.T) {
-    var a, b, prime *big.Int
-    a.SetString("11260647941622813594563746375280766662237311019551239924981511729608487775604310196863705127454617186486639011517352066501847110680463498585797912894788", 10)
-    prime.SetString("800000000000011000000000000000000000000000000000000000000000001", 16)
+	var a, b, prime *big.Int
+	a.SetString("11260647941622813594563746375280766662237311019551239924981511729608487775604310196863705127454617186486639011517352066501847110680463498585797912894788", 10)
+	prime.SetString("800000000000011000000000000000000000000000000000000000000000001", 16)
 
-    _, err := DivMod(a, b, prime)
-    if err == nil {
-        t.Errorf("DivMod expected to fail with division by zero")
-    }
+	_, err := DivMod(a, b, prime)
+	if err == nil {
+		t.Errorf("DivMod expected to fail with division by zero")
+	}
 }
 
 func isqrt_ok(t *testing.T) {
-    var x, y *big.Int
-    x.SetString("4573659632505831259480", 10)
-    y.Mul(x, x)
+	var x, y *big.Int
+	x.SetString("4573659632505831259480", 10)
+	y.Mul(x, x)
 
-    sqr_y, err := ISqrt(y)
-    if err != nil {
-        t.Errorf("ISqrt failed with error: %s", err)
-    }
-    if x.Cmp(sqr_y) != 0 {
-        t.Errorf("Failed to get square root of x^2, x: %s", x)
-    }
+	sqr_y, err := ISqrt(y)
+	if err != nil {
+		t.Errorf("ISqrt failed with error: %s", err)
+	}
+	if x.Cmp(sqr_y) != 0 {
+		t.Errorf("Failed to get square root of x^2, x: %s", x)
+	}
 }
 
 func isqrt_fail(t *testing.T) {
-    x := big.NewInt(-1)
+	x := big.NewInt(-1)
 
-    _, err := ISqrt(x)
-    if err == nil {
-        t.Errorf("expected ISqrt to fail")
-    }
+	_, err := ISqrt(x)
+	if err == nil {
+		t.Errorf("expected ISqrt to fail")
+	}
 }
-
