092e658f8a73608e8cf649f888953d9b3679bc8d
juan.mv <juan@lambdaclass.com>
2023-09-12 12:51:28 -0300

    make fmt

--- pkg/math_utils/utils.go
+++ pkg/math_utils/utils.go
@@ -1,18 +1,18 @@
 package math_utils
 
 import (
-    "math/big"
-    "github.com/pkg/errors"
+	"github.com/pkg/errors"
+	"math/big"
 )
 
 // Finds a nonnegative integer x < p such that (m * x) % p == n.
 func DivMod(n *big.Int, m *big.Int, p *big.Int) (*big.Int, error) {
-    if m.BitLen() == 0 {
-        return nil, errors.Errorf("m in div_mod(n, m, p) can't be zero")
-    }
-    var inv_m, res *big.Int
-    inv_m.ModInverse(m, p)
-    res.Mul(inv_m, n)
-    res.Mod(res, p)
-    return res, nil
+	if m.BitLen() == 0 {
+		return nil, errors.Errorf("m in div_mod(n, m, p) can't be zero")
+	}
+	var inv_m, res *big.Int
+	inv_m.ModInverse(m, p)
+	res.Mul(inv_m, n)
+	res.Mod(res, p)
+	return res, nil
 }
