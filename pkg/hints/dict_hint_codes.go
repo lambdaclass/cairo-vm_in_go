ceeafc0062db598038a2ca8923611b162da4922a
Federica <fedemoletta@hotmail.com>
2023-09-13 13:17:20 -0300

    Implement defaultDictNew

---
+++ pkg/hints/dict_hint_codes.go
@@ -0,0 +1,3 @@
+package hints
+
+const DEFAULT_DICT_NEW = "if '__dict_manager' not in globals():\n    from starkware.cairo.common.dict import DictManager\n    __dict_manager = DictManager()\n\nmemory[ap] = __dict_manager.new_default_dict(segments, ids.default_value)"
---
+++ pkg/hints/dict_hints.go
@@ -0,0 +1,41 @@
+package hints
+
+import (
+	. "github.com/lambdaclass/cairo-vm.go/pkg/hints/dict_manager"
+	. "github.com/lambdaclass/cairo-vm.go/pkg/hints/hint_utils"
+	. "github.com/lambdaclass/cairo-vm.go/pkg/types"
+	. "github.com/lambdaclass/cairo-vm.go/pkg/vm"
+	"github.com/lambdaclass/cairo-vm.go/pkg/vm/memory"
+)
+
+// func copyInitialDict(scopes ExecutionScopes) (*map[MaybeRelocatable]MaybeRelocatable, error) {
+// 	initial_dict_any, err := scopes.Get("initial_dict")
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	initial_dict, ok := initial_dict_any.(map[MaybeRelocatable]MaybeRelocatable)
+// }
+
+func fetchDictManager(scopes *ExecutionScopes) (*DictManager, bool) {
+	dictManager, err := scopes.Get("__dict_manager")
+	if err != nil {
+		return nil, false
+	}
+	val, ok := dictManager.(*DictManager)
+	return val, ok
+}
+
+func defaultDictNew(ids IdsManager, scopes *ExecutionScopes, vm *VirtualMachine) error {
+	defaultValue, err := ids.Get("default_value", vm)
+	if err != nil {
+		return err
+	}
+	dictManager, ok := fetchDictManager(scopes)
+	if !ok {
+		newDictManager := NewDictManager()
+		dictManager = &newDictManager
+		scopes.AssignOrUpdateVariable("__dict_manager", dictManager)
+	}
+	base := dictManager.NewDefaultDictionary(defaultValue, vm)
+	return vm.Segments.Memory.Insert(vm.RunContext.Ap, memory.NewMaybeRelocatableRelocatable(base))
+}
