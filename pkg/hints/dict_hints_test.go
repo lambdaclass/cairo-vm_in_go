ba629309a8ae6874b18cb99c45a4b6c53f1ea637
Federica <fedemoletta@hotmail.com>
2023-09-13 14:05:09 -0300

    Add test

--- pkg/hints/dict_hints_test.go
+++ pkg/hints/dict_hints_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	. "github.com/lambdaclass/cairo-vm.go/pkg/hints"
+	"github.com/lambdaclass/cairo-vm.go/pkg/hints/dict_manager"
 	. "github.com/lambdaclass/cairo-vm.go/pkg/hints/hint_utils"
 	. "github.com/lambdaclass/cairo-vm.go/pkg/lambdaworks"
 	"github.com/lambdaclass/cairo-vm.go/pkg/types"
@@ -36,6 +37,43 @@ func TestDefaultDictNewCreateManager(t *testing.T) {
 	_, ok := FetchDictManager(scopes)
 	if !ok {
 		t.Error("DEFAULT_DICT_NEW No DictManager created")
+	}
+	// Check that the correct base was inserted into ap
+	val, _ := vm.Segments.Memory.Get(vm.RunContext.Ap)
+	if val == nil || *val != *NewMaybeRelocatableRelocatable(NewRelocatable(1, 0)) {
+		t.Error("DEFAULT_DICT_NEW Wrong/No base inserted into ap")
+	}
+}
+
+func TestDefaultDictNewHasManager(t *testing.T) {
+	vm := NewVirtualMachine()
+	scopes := types.NewExecutionScopes()
+	// Create dictManager & add it to scope
+	dictManager := dict_manager.NewDictManager()
+	dictManagerRef := &dictManager
+	scopes.AssignOrUpdateVariable("__dict_manager", dictManagerRef)
+	vm.Segments.AddSegment()
+	idsManager := SetupIdsForTest(
+		map[string][]*MaybeRelocatable{
+			"default_value": {NewMaybeRelocatableFelt(FeltFromUint64(17))},
+		},
+		vm,
+	)
+	hintProcessor := CairoVmHintProcessor{}
+	hintData := any(HintData{
+		Ids:  idsManager,
+		Code: DEFAULT_DICT_NEW,
+	})
+	// Advance AP so that values don't clash with FP-based ids
+	vm.RunContext.Ap = NewRelocatable(0, 5)
+	err := hintProcessor.ExecuteHint(vm, &hintData, nil, scopes)
+	if err != nil {
+		t.Errorf("DEFAULT_DICT_NEW hint test failed with error %s", err)
+	}
+	// Check that the manager wan't replaced by a new one
+	dictManagerPtr, ok := FetchDictManager(scopes)
+	if !ok || dictManagerPtr != dictManagerRef {
+		t.Error("DEFAULT_DICT_NEW DictManager replaced")
 	}
 	// Check that the correct base was inserted into ap
 	val, _ := vm.Segments.Memory.Get(vm.RunContext.Ap)
