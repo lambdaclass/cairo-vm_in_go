0145060d08775816cbf666dbd6eb326572ec6865
Milton <milton.scuderi@lambdaclass.com>
2023-09-13 16:13:00 -0300

    Fix error type across platforms

--- pkg/lambdaworks/lambdaworks.go
+++ pkg/lambdaworks/lambdaworks.go
@@ -230,7 +230,7 @@ func (a Felt) Shl(num uint64) Felt {
 	var result C.felt_t
 	var a_c C.felt_t = a.toC()
 
-	C.felt_shl(&a_c[0], C.ulonglong(num), &result[0])
+	C.felt_shl(&a_c[0], C.uint64_t(num), &result[0])
 	return fromC(result)
 }
 
