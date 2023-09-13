effc2076aa34f8ff33b725383536ce1de03f7d67
Federica <fedemoletta@hotmail.com>
2023-09-13 18:10:59 -0300

    Add integration test

--- pkg/vm/cairo_run/cairo_run_test.go
+++ pkg/vm/cairo_run/cairo_run_test.go
@@ -113,3 +113,10 @@ func TestDict(t *testing.T) {
 		t.Errorf("Program execution failed with error: %s", err)
 	}
 }
+
+func TestDictUpdate(t *testing.T) {
+	_, err := cairo_run.CairoRun("../../../cairo_programs/dict_update.json", "small", false)
+	if err != nil {
+		t.Errorf("Program execution failed with error: %s", err)
+	}
+}
