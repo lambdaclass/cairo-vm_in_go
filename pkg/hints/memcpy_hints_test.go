791dedca8870666d8f73231e52dda846159b1061
toni-calvin <antonio.calvin@lambdaclass.com>
2023-09-15 15:24:05 +0200

    add test

--- pkg/hints/memcpy_hints_test.go
+++ pkg/hints/memcpy_hints_test.go
@@ -5,6 +5,7 @@ import (
 
 	. "github.com/lambdaclass/cairo-vm.go/pkg/hints"
 	. "github.com/lambdaclass/cairo-vm.go/pkg/hints/hint_utils"
+	"github.com/lambdaclass/cairo-vm.go/pkg/lambdaworks"
 	. "github.com/lambdaclass/cairo-vm.go/pkg/lambdaworks"
 	. "github.com/lambdaclass/cairo-vm.go/pkg/types"
 	. "github.com/lambdaclass/cairo-vm.go/pkg/vm"
@@ -78,4 +79,34 @@ func TestExitScopeInvalid(t *testing.T) {
 		t.Errorf("TestExitScopeInvalid should fail with error %s", ErrCannotExitMainScop)
 	}
 
+}
+
+func TestMemcpyEnterScopeHintValid(t *testing.T) {
+	vm := NewVirtualMachine()
+	vm.Segments.AddSegment()
+	vm.Segments.AddSegment()
+	idsManager := SetupIdsForTest(
+		map[string][]*MaybeRelocatable{
+			"len": {NewMaybeRelocatableFelt(FeltFromUint64(45))},
+		},
+		vm,
+	)
+	hintProcessor := CairoVmHintProcessor{}
+	hintData := any(HintData{
+		Ids:  idsManager,
+		Code: MEMCPY_ENTER_SCOPE,
+	})
+
+	executionScopes := NewExecutionScopes()
+	err := hintProcessor.ExecuteHint(vm, &hintData, nil, executionScopes)
+	if err != nil {
+		t.Errorf("TestMemcpyEnterScopeHintValid failed with error %s", err)
+	}
+	res, err := executionScopes.Get("n")
+	if err != nil {
+		t.Errorf("TestMemcpyEnterScopeHintValid failed with error %s", err)
+	}
+	if res.(lambdaworks.Felt) != lambdaworks.FeltFromDecString("45") {
+		t.Errorf("TestMemcpyEnterScopeHintValid failed, expected len: %d, got: %d", lambdaworks.FeltFromDecString("45"), res.(lambdaworks.Felt))
+	}
 }
