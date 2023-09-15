c52db8dcf4cc191199f39314a0f79ad943e671bb
Antonio Calvín García <garcia.calvin.toni@gmail.com>
2023-09-15 21:34:46 +0200

    `Memcpy enter scope` hint (#226)
    * implement hint
    
    * add test
    
    * add test
    
    * improve readability
    
    Co-authored-by: fmoletta <99273364+fmoletta@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: fmoletta <99273364+fmoletta@users.noreply.github.com>
    Co-authored-by: Mariano Nicolini <mariano.nicolini.91@gmail.com>

--- pkg/hints/hint_processor.go
+++ pkg/hints/hint_processor.go
@@ -57,6 +57,8 @@ func (p *CairoVmHintProcessor) ExecuteHint(vm *vm.VirtualMachine, hintData *any,
 		return vm_exit_scope(execScopes)
 	case ASSERT_NOT_EQUAL:
 		return assert_not_equal(data.Ids, vm)
+	case MEMCPY_ENTER_SCOPE:
+		return memcpy_enter_scope(data.Ids, vm, execScopes)
 	case VM_ENTER_SCOPE:
 		return vm_enter_scope(execScopes)
 	default:
