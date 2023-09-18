1eabf940f324e7688eb5451c322e1a98b891dc10
Federica <fedemoletta@hotmail.com>
2023-09-18 18:46:46 -0300

    Add integration test

--- pkg/vm/cairo_run/cairo_run_test.go
+++ pkg/vm/cairo_run/cairo_run_test.go
@@ -202,3 +202,11 @@ func TestSqrtHint(t *testing.T) {
 		t.Errorf("Program execution failed with error: %s", err)
 	}
 }
+
+func TestAssertLeFelt(t *testing.T) {
+	cairoRunConfig := cairo_run.CairoRunConfig{DisableTracePadding: false, Layout: "all_cairo", ProofMode: false}
+	_, err := cairo_run.CairoRun("../../../cairo_programs/assert_le_felt.json", cairoRunConfig)
+	if err != nil {
+		t.Errorf("Program execution failed with error: %s", err)
+	}
+}
