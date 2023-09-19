3611a3843231bbe99ccd79f8f1cdbb555b86a6c5
toni-calvin <antonio.calvin@lambdaclass.com>
2023-09-19 18:03:07 +0200

    improve tests

--- pkg/hints/memcpy_hints_test.go
+++ pkg/hints/memcpy_hints_test.go
@@ -50,7 +50,7 @@ func TestExitScopeHintValid(t *testing.T) {
 
 	err := hintProcessor.ExecuteHint(vm, &hintData, nil, executionScopes)
 	if err != nil {
-		t.Errorf("TestExitScopeHintValid failed with error %s", err)
+		t.Errorf("failed with error %s", err)
 	}
 
 }
@@ -76,7 +76,7 @@ func TestExitScopeHintInvalid(t *testing.T) {
 
 	err := hintProcessor.ExecuteHint(vm, &hintData, nil, executionScopes)
 	if err.Error() != ErrCannotExitMainScop.Error() {
-		t.Errorf("TestExitScopeHintInvalid should fail with error %s", ErrCannotExitMainScop)
+		t.Errorf("should fail with error %s", ErrCannotExitMainScop)
 	}
 
 }
@@ -100,14 +100,14 @@ func TestMemcpyEnterScopeHintValid(t *testing.T) {
 	executionScopes := NewExecutionScopes()
 	err := hintProcessor.ExecuteHint(vm, &hintData, nil, executionScopes)
 	if err != nil {
-		t.Errorf("TestMemcpyEnterScopeHintValid failed with error %s", err)
+		t.Errorf("failed with error %s", err)
 	}
 	res, err := executionScopes.Get("n")
 	if err != nil {
-		t.Errorf("TestMemcpyEnterScopeHintValid failed with error %s", err)
+		t.Errorf("failed with error %s", err)
 	}
 	if res.(Felt) != FeltFromDecString("45") {
-		t.Errorf("TestMemcpyEnterScopeHintValid failed, expected len: %d, got: %d", FeltFromDecString("45"), res.(Felt))
+		t.Errorf("failed, expected len: %d, got: %d", FeltFromDecString("45"), res.(Felt))
 	}
 }
 
@@ -129,7 +129,7 @@ func TestMemcpyEnterScopeHintInvalid(t *testing.T) {
 	executionScopes := NewExecutionScopes()
 	err := hintProcessor.ExecuteHint(vm, &hintData, nil, executionScopes)
 	if err.Error() != ErrUnknownIdentifier("len").Error() {
-		t.Errorf("TestMemcpyEnterScopeHintInvalid should fail with error %s", ErrUnknownIdentifier("len"))
+		t.Errorf("should fail with error %s", ErrUnknownIdentifier("len"))
 	}
 }
 
@@ -152,20 +152,15 @@ func TestEnterScope(t *testing.T) {
 
 	err := hintProcessor.ExecuteHint(vm, &hintData, nil, executionScopes)
 	if err != nil {
-		t.Errorf("TestEnterScopeHint failed with error %s", err)
+		t.Errorf("failed with error %s", err)
 	}
 }
 
-func TestMemcpyContinueCopyingValid(t *testing.T) {
+func TestMemcpyContinueCopyingValid1(t *testing.T) {
 	vm := NewVirtualMachine()
-	vm.Segments = AddNSegments(vm.Segments, 3)
-	vm.RunContext.Fp = NewRelocatable(2, 0)
-	vm.Segments.Memory.Data[NewRelocatable(1, 2)] = *NewMaybeRelocatableFelt(FeltFromUint64(5))
-
-	executionScopes := NewExecutionScopes()
-	scope := make(map[string]interface{})
-	scope["n"] = FeltOne()
-	executionScopes.EnterScope(scope)
+	vm.Segments = AddNSegments(vm.Segments, 2)
+	vm.RunContext.Fp = NewRelocatable(1, 2)
+	executionScopes := NewExecutionScopesWithInitValue("n", FeltOne())
 
 	idsManager := SetupIdsForTest(
 		map[string][]*MaybeRelocatable{
@@ -181,7 +176,47 @@ func TestMemcpyContinueCopyingValid(t *testing.T) {
 
 	err := hintProcessor.ExecuteHint(vm, &hintData, nil, executionScopes)
 	if err != nil {
-		t.Errorf("TestMemsetContinueLoopValidEqual1 failed with error %s", err)
+		t.Errorf("failed with error %s", err)
+	}
+
+	val, err := idsManager.GetFelt("continue_copying", vm)
+	if err != nil {
+		t.Errorf("failed with error %s", err)
+	}
+	if val != FeltZero() {
+		t.Errorf("failed, expected val: %d, got: %d", FeltZero(), val)
+	}
+}
+
+func TestMemcpyContinueCopyingValidNeg1(t *testing.T) {
+	vm := NewVirtualMachine()
+	vm.Segments = AddNSegments(vm.Segments, 2)
+	vm.RunContext.Fp = NewRelocatable(1, 2)
+	executionScopes := NewExecutionScopesWithInitValue("n", FeltFromDecString("-1"))
+
+	idsManager := SetupIdsForTest(
+		map[string][]*MaybeRelocatable{
+			"continue_copying": nil,
+		},
+		vm,
+	)
+	hintProcessor := CairoVmHintProcessor{}
+	hintData := any(HintData{
+		Ids:  idsManager,
+		Code: MEMCPY_CONTINUE_COPYING,
+	})
+
+	err := hintProcessor.ExecuteHint(vm, &hintData, nil, executionScopes)
+	if err != nil {
+		t.Errorf("failed with error %s", err)
+	}
+
+	val, err := idsManager.GetFelt("continue_copying", vm)
+	if err != nil {
+		t.Errorf("failed with error %s", err)
+	}
+	if val != FeltOne() {
+		t.Errorf("failed, expected val: %d, got: %d", FeltOne(), val)
 	}
 }
 
@@ -207,7 +242,7 @@ func TestMemcpyContinueCopyingVarNotInScope(t *testing.T) {
 
 	err := hintProcessor.ExecuteHint(vm, &hintData, nil, executionScopes)
 	if err.Error() != ErrVariableNotInScope("n").Error() {
-		t.Errorf("TestMemcpyContinueCopyingVarNotInScope should fail with error %s", ErrVariableNotInScope("n"))
+		t.Errorf("should fail with error %s", ErrVariableNotInScope("n"))
 	}
 }
 
@@ -235,6 +270,6 @@ func TestMemcpyContinueCopyingInsertError(t *testing.T) {
 	err := hintProcessor.ExecuteHint(vm, &hintData, nil, executionScopes)
 	expected := ErrMemoryWriteOnce(NewRelocatable(0, 0), *NewMaybeRelocatableFeltFromUint64(5), *NewMaybeRelocatableFeltFromUint64(0))
 	if err.Error() != expected.Error() {
-		t.Errorf("TestMemcpyContinueCopyingInsertError should fail with error %s", expected)
+		t.Errorf("should fail with error %s", expected)
 	}
 }
