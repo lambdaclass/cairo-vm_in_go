e969f48fc98b27b9374705d2e10b15e2d5054d8b
Federica <fedemoletta@hotmail.com>
2023-09-19 16:51:10 -0300

    Add unit tests

--- pkg/hints/keccak_hints_test.go
+++ pkg/hints/keccak_hints_test.go
@@ -33,7 +33,7 @@ func TestUnsafeKeccakOk(t *testing.T) {
 	vm.Segments.LoadData(data_ptr, &data)
 	// Add __keccak_max_size
 	scopes := NewExecutionScopes()
-	scopes.AssignOrUpdateVariable("__keccak_max_size", uint16(500))
+	scopes.AssignOrUpdateVariable("__keccak_max_size", uint64(500))
 	hintProcessor := CairoVmHintProcessor{}
 	hintData := any(HintData{
 		Ids:  idsManager,
@@ -53,5 +53,72 @@ func TestUnsafeKeccakOk(t *testing.T) {
 	expectedLow := FeltFromDecString("259413678945892999811634722593932702747")
 	if err != nil || low != expectedLow {
 		t.Errorf("Wrong/No ids.low\n Expected %s, got %s.", expectedLow.ToHexString(), low.ToHexString())
+	}
+}
+
+func TestUnsafeKeccakMaxSizeExceeded(t *testing.T) {
+	vm := NewVirtualMachine()
+	vm.Segments.AddSegment()
+	data_ptr := vm.Segments.AddSegment()
+	idsManager := SetupIdsForTest(
+		map[string][]*MaybeRelocatable{
+			"length": {NewMaybeRelocatableFelt(FeltFromUint64(3))},
+			"data":   {NewMaybeRelocatableRelocatable(data_ptr)},
+			"high":   {nil},
+			"low":    {nil},
+		},
+		vm,
+	)
+	// Insert data into memory
+	data := []MaybeRelocatable{
+		*NewMaybeRelocatableFelt(FeltOne()),
+		*NewMaybeRelocatableFelt(FeltOne()),
+		*NewMaybeRelocatableFelt(FeltOne()),
+	}
+	vm.Segments.LoadData(data_ptr, &data)
+	// Add __keccak_max_size
+	scopes := NewExecutionScopes()
+	scopes.AssignOrUpdateVariable("__keccak_max_size", uint64(2))
+	hintProcessor := CairoVmHintProcessor{}
+	hintData := any(HintData{
+		Ids:  idsManager,
+		Code: UNSAFE_KECCAK,
+	})
+	err := hintProcessor.ExecuteHint(vm, &hintData, nil, scopes)
+	if err == nil {
+		t.Errorf("UNSAFE_KECCAK hint test should have failed")
+	}
+}
+
+func TestUnsafeKeccakInvalidWordSize(t *testing.T) {
+	vm := NewVirtualMachine()
+	vm.Segments.AddSegment()
+	data_ptr := vm.Segments.AddSegment()
+	idsManager := SetupIdsForTest(
+		map[string][]*MaybeRelocatable{
+			"length": {NewMaybeRelocatableFelt(FeltFromUint64(3))},
+			"data":   {NewMaybeRelocatableRelocatable(data_ptr)},
+			"high":   {nil},
+			"low":    {nil},
+		},
+		vm,
+	)
+	// Insert data into memory
+	data := []MaybeRelocatable{
+		*NewMaybeRelocatableFelt(FeltFromDecString("-1")),
+		*NewMaybeRelocatableFelt(FeltOne()),
+		*NewMaybeRelocatableFelt(FeltOne()),
+	}
+	vm.Segments.LoadData(data_ptr, &data)
+	// Add __keccak_max_size
+	scopes := NewExecutionScopes()
+	hintProcessor := CairoVmHintProcessor{}
+	hintData := any(HintData{
+		Ids:  idsManager,
+		Code: UNSAFE_KECCAK,
+	})
+	err := hintProcessor.ExecuteHint(vm, &hintData, nil, scopes)
+	if err == nil {
+		t.Errorf("UNSAFE_KECCAK hint test should have failed")
 	}
 }
