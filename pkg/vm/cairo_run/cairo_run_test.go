7a21bfbc15544944242f94c00c516de48b886a58
Pedro Fontana <fontana.pedro93@gmail.com>
2023-09-21 19:52:15 -0300

    integration tests

--- pkg/vm/cairo_run/cairo_run_test.go
+++ pkg/vm/cairo_run/cairo_run_test.go
@@ -185,3 +185,11 @@ func TestDictSquash(t *testing.T) {
 func TestSplitFeltHint(t *testing.T) {
 	testProgram("split_felt", t)
 }
+
+func TestUsort(t *testing.T) {
+	testProgram("usort", t)
+}
+
+func TestUsortProofMode(t *testing.T) {
+	testProgramProof("usort", t)
+}
