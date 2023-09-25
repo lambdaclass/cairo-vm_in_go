cef26c00e0e94a75eedf1846a9e0d6f607acd778
Federica <fedemoletta@hotmail.com>
2023-09-25 18:16:24 -0300

    Add hint codes

---
+++ pkg/hints/hint_codes/signature_hint_codes.go
@@ -0,0 +1,15 @@
+package hint_codes
+
+const DIV_MOD_N_PACKED_DIVMOD_V1 = `from starkware.cairo.common.cairo_secp.secp_utils import N, pack
+from starkware.python.math_utils import div_mod, safe_div
+
+a = pack(ids.a, PRIME)
+b = pack(ids.b, PRIME)
+value = res = div_mod(a, b, N)`
+
+const DIV_MOD_N_PACKED_DIVMOD_EXTERNAL_N = `from starkware.cairo.common.cairo_secp.secp_utils import pack
+from starkware.python.math_utils import div_mod, safe_div
+
+a = pack(ids.a, PRIME)
+b = pack(ids.b, PRIME)
+value = res = div_mod(a, b, N)`
