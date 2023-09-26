8cd64b6a6f690fcbdc6617be5bf956658bfc24af
Federica <fedemoletta@hotmail.com>
2023-09-26 14:01:35 -0300

    Add unit test

--- pkg/hints/signature_hints_test.go
+++ pkg/hints/signature_hints_test.go
@@ -54,3 +54,46 @@ func TestDivModNPackedDivMod(t *testing.T) {
 		t.Error("Wrong/No scope value val")
 	}
 }
+
+func TestDivModNPackedDivModExternalN(t *testing.T) {
+	vm := NewVirtualMachine()
+	vm.Segments.AddSegment()
+	idsManager := SetupIdsForTest(
+		map[string][]*MaybeRelocatable{
+			"a": {
+				NewMaybeRelocatableFelt(FeltFromUint64(20)),
+				NewMaybeRelocatableFelt(FeltFromUint64(0)),
+				NewMaybeRelocatableFelt(FeltFromUint64(0)),
+			},
+			"b": {
+				NewMaybeRelocatableFelt(FeltFromUint64(2)),
+				NewMaybeRelocatableFelt(FeltFromUint64(0)),
+				NewMaybeRelocatableFelt(FeltFromUint64(0)),
+			},
+		},
+		vm,
+	)
+	hintProcessor := CairoVmHintProcessor{}
+	hintData := any(HintData{
+		Ids:  idsManager,
+		Code: DIV_MOD_N_PACKED_DIVMOD_EXTERNAL_N,
+	})
+	scopes := NewExecutionScopes()
+	scopes.AssignOrUpdateVariable("N", big.NewInt(7))
+	err := hintProcessor.ExecuteHint(vm, &hintData, nil, scopes)
+	if err != nil {
+		t.Errorf("DIV_MOD_N_PACKED_DIVMOD_EXTERNAL_N hint test failed with error %s", err)
+	}
+	// Check result in scope
+	expectedRes := big.NewInt(3)
+
+	res, err := scopes.Get("res")
+	if err != nil || res.(*big.Int).Cmp(expectedRes) != 0 {
+		t.Error("Wrong/No scope value res")
+	}
+
+	val, err := scopes.Get("val")
+	if err != nil || val.(*big.Int).Cmp(expectedRes) != 0 {
+		t.Error("Wrong/No scope value val")
+	}
+}
