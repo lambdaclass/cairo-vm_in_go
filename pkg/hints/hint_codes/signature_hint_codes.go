f3d9dbed8dd53c2321d83fb8346f522b137d033c
Federica <fedemoletta@hotmail.com>
2023-09-26 14:13:25 -0300

    Add hint codes

--- pkg/hints/hint_codes/signature_hint_codes.go
+++ pkg/hints/hint_codes/signature_hint_codes.go
@@ -13,3 +13,7 @@ from starkware.python.math_utils import div_mod, safe_div
 a = pack(ids.a, PRIME)
 b = pack(ids.b, PRIME)
 value = res = div_mod(a, b, N)`
+
+const DIV_MOD_N_SAFE_DIV = "value = k = safe_div(res * b - a, N)"
+
+const DIV_MOD_N_SAFE_DIV_PLUS_ONE = "value = k_plus_one = safe_div(res * b - a, N) + 1"
