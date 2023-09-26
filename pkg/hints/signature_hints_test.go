97c9cc7fa5e83a94cc8ceb76412ecdf902b8aa67
Federica <fedemoletta@hotmail.com>
2023-09-26 16:14:37 -0300

    Add unit test

--- pkg/hints/signature_hints_test.go
+++ pkg/hints/signature_hints_test.go
@@ -128,3 +128,34 @@ func TestDivModSafeDivOk(t *testing.T) {
 		t.Error("Wrong/No scope variable value")
 	}
 }
+
+func TestDivModSafeDivPlusOneOk(t *testing.T) {
+	vm := NewVirtualMachine()
+	vm.Segments.AddSegment()
+	idsManager := SetupIdsForTest(
+		map[string][]*MaybeRelocatable{},
+		vm,
+	)
+	hintProcessor := CairoVmHintProcessor{}
+	hintData := any(HintData{
+		Ids:  idsManager,
+		Code: DIV_MOD_N_SAFE_DIV_PLUS_ONE,
+	})
+	scopes := NewExecutionScopes()
+	scopes.AssignOrUpdateVariable("N", big.NewInt(5))
+	scopes.AssignOrUpdateVariable("a", big.NewInt(10))
+	scopes.AssignOrUpdateVariable("b", big.NewInt(30))
+	scopes.AssignOrUpdateVariable("res", big.NewInt(2))
+	err := hintProcessor.ExecuteHint(vm, &hintData, nil, scopes)
+	if err != nil {
+		t.Errorf("DIV_MOD_N_SAFE_DIV_PLUS_ONE hint test failed with error %s", err)
+	}
+	// Check result in scope
+	expectedValue := big.NewInt(11) // (2 * 30 - 10) / 5 + 1 = 11
+
+	value, err := scopes.Get("value")
+	println(value.(*big.Int).Text(10))
+	if err != nil || value.(*big.Int).Cmp(expectedValue) != 0 {
+		t.Error("Wrong/No scope variable value")
+	}
+}
