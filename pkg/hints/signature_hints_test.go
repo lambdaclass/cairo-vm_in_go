b542a09e6f135e9cc6011c87c62507cd344d4aee
Federica <fedemoletta@hotmail.com>
2023-09-26 13:59:02 -0300

    Add unit test

---
+++ pkg/hints/signature_hints_test.go
@@ -0,0 +1,56 @@
+package hints_test
+
+import (
+	"math/big"
+	"testing"
+
+	. "github.com/lambdaclass/cairo-vm.go/pkg/hints"
+	. "github.com/lambdaclass/cairo-vm.go/pkg/hints/hint_codes"
+	. "github.com/lambdaclass/cairo-vm.go/pkg/hints/hint_utils"
+	. "github.com/lambdaclass/cairo-vm.go/pkg/lambdaworks"
+	. "github.com/lambdaclass/cairo-vm.go/pkg/types"
+	. "github.com/lambdaclass/cairo-vm.go/pkg/vm"
+	. "github.com/lambdaclass/cairo-vm.go/pkg/vm/memory"
+)
+
+func TestDivModNPackedDivMod(t *testing.T) {
+	vm := NewVirtualMachine()
+	vm.Segments.AddSegment()
+	idsManager := SetupIdsForTest(
+		map[string][]*MaybeRelocatable{
+			"a": {
+				NewMaybeRelocatableFelt(FeltFromUint64(10)),
+				NewMaybeRelocatableFelt(FeltFromUint64(0)),
+				NewMaybeRelocatableFelt(FeltFromUint64(0)),
+			},
+			"b": {
+				NewMaybeRelocatableFelt(FeltFromUint64(2)),
+				NewMaybeRelocatableFelt(FeltFromUint64(0)),
+				NewMaybeRelocatableFelt(FeltFromUint64(0)),
+			},
+		},
+		vm,
+	)
+	hintProcessor := CairoVmHintProcessor{}
+	hintData := any(HintData{
+		Ids:  idsManager,
+		Code: DIV_MOD_N_PACKED_DIVMOD_V1,
+	})
+	scopes := NewExecutionScopes()
+	err := hintProcessor.ExecuteHint(vm, &hintData, nil, scopes)
+	if err != nil {
+		t.Errorf("DIV_MOD_N_PACKED_DIVMOD_V1 hint test failed with error %s", err)
+	}
+	// Check result in scope
+	expectedRes := big.NewInt(5)
+
+	res, err := scopes.Get("res")
+	if err != nil || res.(*big.Int).Cmp(expectedRes) != 0 {
+		t.Error("Wrong/No scope value res")
+	}
+
+	val, err := scopes.Get("val")
+	if err != nil || val.(*big.Int).Cmp(expectedRes) != 0 {
+		t.Error("Wrong/No scope value val")
+	}
+}
