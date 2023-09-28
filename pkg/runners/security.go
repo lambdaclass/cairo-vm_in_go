324fc1de978ac0960e37563fbadabc683ac99cb5
Federica <fedemoletta@hotmail.com>
2023-09-28 15:35:40 -0300

    Finish verifySecureRunner

--- pkg/runners/security.go
+++ pkg/runners/security.go
@@ -1,5 +1,10 @@
 package runners
 
+import (
+	"github.com/lambdaclass/cairo-vm.go/pkg/builtins"
+	"github.com/pkg/errors"
+)
+
 /*
 Verify that the completed run in a runner is safe to be relocated and be
 used by other Cairo programs.
@@ -14,16 +19,50 @@ Checks include:
 Note: Each builtin is responsible for checking its own segments' data.
 */
 func VerifySecureRunner(runner *CairoRunner, verifyBuiltins bool, programSegmentSize *uint) error {
-	// programSize := uint(len(runner.Program.Data))
-	// if programSegmentSize != nil {
-	// 	programSize = *programSegmentSize
-	// }
-	// builtinSegmentInfo := make(map[string]uint)
-	// if verifyBuiltins {
-	// 	for i := 0; i < len(runner.Vm.BuiltinRunners); i++ {
-	// 		info := runner.Vm.BuiltinRunners[i].GetSegmentInfo
-	// 	}
-	// }
+	programSize := uint(len(runner.Program.Data))
+	if programSegmentSize != nil {
+		programSize = *programSegmentSize
+	}
+	// Get builtin segment info
+	builtinNames := make(map[int]string)
+	builtinSizes := make(map[int]uint)
+	if verifyBuiltins {
+		for i := 0; i < len(runner.Vm.BuiltinRunners); i++ {
+			base, stopPtr, err := runner.Vm.BuiltinRunners[i].GetMemorySegmentAddresses()
+			if err != nil {
+				return err
+			}
+			builtinNames[base.SegmentIndex] = runner.Vm.BuiltinRunners[i].Name()
+			builtinSizes[base.SegmentIndex] = stopPtr.Offset
+		}
+	}
+	// Run memory checks
+	for addr, val := range runner.Vm.Segments.Memory.Data {
+		// Check out of bound accesses to builtin segment
+		size, ok := builtinSizes[addr.SegmentIndex]
+		if ok && addr.Offset >= size {
+			return errors.Errorf("Out of bounds access to builtin segment %s at %s", builtinNames[addr.SegmentIndex], addr.ToString())
+		}
+		// Check out of bound accesses to program segment
+		if addr.SegmentIndex == runner.ProgramBase.SegmentIndex && addr.SegmentIndex >= int(programSize) {
+			return errors.Errorf("Out of bounds access to program segment at %s", addr.ToString())
+		}
+		// Check non-relocated temporary addresses
+		if addr.SegmentIndex < 0 {
+			return errors.Errorf("Security Error: Invalid Memory Value: temporary address not relocated: %s", addr.ToString())
+		}
+		relVal, isRel := val.GetRelocatable()
+		if isRel && relVal.SegmentIndex < 0 {
+			return errors.Errorf("Security Error: Invalid Memory Value: temporary address not relocated: %s", relVal.ToString())
+		}
+	}
+	// Run builtin-specific checks
+	for i := 0; i < len(runner.Vm.BuiltinRunners); i++ {
+		err := builtins.RunSecurityChecksForBuiltin(runner.Vm.BuiltinRunners[i], &runner.Vm.Segments)
+		if err != nil {
+			return err
+		}
+	}
 
 	return nil
 }
