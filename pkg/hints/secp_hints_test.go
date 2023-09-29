94b046c7354f8d8a08a0a2a3f537308e4d6bbabc
Federica <fedemoletta@hotmail.com>
2023-09-29 11:48:26 -0300

    Add unit test

--- pkg/hints/secp_hints_test.go
+++ pkg/hints/secp_hints_test.go
@@ -1,6 +1,7 @@
 package hints_test
 
 import (
+	"math/big"
 	"testing"
 
 	. "github.com/lambdaclass/cairo-vm.go/pkg/hints"
@@ -12,4 +13,43 @@ import (
 	. "github.com/lambdaclass/cairo-vm.go/pkg/vm/memory"
 )
 
-func TestReduceV1(t *testing.T) {}
+func TestReduceV1(t *testing.T) {
+	vm := NewVirtualMachine()
+	vm.Segments.AddSegment()
+	idsManager := SetupIdsForTest(
+		map[string][]*MaybeRelocatable{
+			"x": {
+				NewMaybeRelocatableFelt(FeltFromDecString("6")),
+				NewMaybeRelocatableFelt(FeltFromDecString("6")),
+				NewMaybeRelocatableFelt(FeltFromDecString("6")),
+			},
+		},
+		vm,
+	)
+	scopes := NewExecutionScopes()
+
+	hintProcessor := CairoVmHintProcessor{}
+	hintData := any(HintData{
+		Ids:  idsManager,
+		Code: REDUCE_V1,
+	})
+
+	err := hintProcessor.ExecuteHint(vm, &hintData, nil, scopes)
+	if err != nil {
+		t.Errorf("REDUCE_V1 hint failed with error %s", err)
+	}
+	// Checked scope variables
+	// SECP_P
+	secpP, err := FetchScopeVar[big.Int]("SECP_P", scopes)
+	expectedSecpP := SECP_P()
+	if err != nil || secpP.Cmp(&expectedSecpP) != 0 {
+		t.Errorf("Wrong/No scope var SECP_P")
+	}
+
+	value, err := FetchScopeVar[big.Int]("value", scopes)
+	valueUnpacked := Uint384{Limbs: []Felt{FeltFromUint(6), FeltFromUint(6), FeltFromUint(6)}}
+	expectedvalue := valueUnpacked.Pack86()
+	if err != nil || value.Cmp(&expectedvalue) != 0 {
+		t.Errorf("Wrong/No scope var value")
+	}
+}
