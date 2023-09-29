ee2b36c8211eb74ff06bb01d3885876073fc697e
Federica <fedemoletta@hotmail.com>
2023-09-29 11:40:03 -0300

    Fix file names

---
+++ pkg/hints/secp_hints_test.go
@@ -0,0 +1,15 @@
+package hints_test
+
+import (
+	"testing"
+
+	. "github.com/lambdaclass/cairo-vm.go/pkg/hints"
+	. "github.com/lambdaclass/cairo-vm.go/pkg/hints/hint_codes"
+	. "github.com/lambdaclass/cairo-vm.go/pkg/hints/hint_utils"
+	. "github.com/lambdaclass/cairo-vm.go/pkg/lambdaworks"
+	. "github.com/lambdaclass/cairo-vm.go/pkg/types"
+	. "github.com/lambdaclass/cairo-vm.go/pkg/vm"
+	. "github.com/lambdaclass/cairo-vm.go/pkg/vm/memory"
+)
+
+func TestReduceV1(t *testing.T) {}
