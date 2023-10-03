bc5f5d4b618753cfb08440b789c8c9bea7457607
Federica <fedemoletta@hotmail.com>
2023-10-03 11:07:45 -0300

    Finish blake2s impl

--- pkg/hints/hint_utils/blake2s_hash.go
+++ pkg/hints/hint_utils/blake2s_hash.go
@@ -31,7 +31,7 @@ func rightRot(value uint32, n uint32) uint32 {
 	return (value >> n) | ((value & ((1 << n) - 1)) << (32 - n))
 }
 
-func mix(a uint32, b uint32, c uint32, d uint32, m0 uint32, m1 uint32) (uint32, uint32, uint32) {
+func mix(a uint32, b uint32, c uint32, d uint32, m0 uint32, m1 uint32) (uint32, uint32, uint32, uint32) {
 	a = a + b + m0
 	d = rightRot(d^a, 16)
 	c = c + d
@@ -40,5 +40,94 @@ func mix(a uint32, b uint32, c uint32, d uint32, m0 uint32, m1 uint32) (uint32, 
 	d = rightRot(d^a, 8)
 	c = c + d
 	b = rightRot(b^c, 7)
-	return a, b, c
+	return a, b, c, d
+}
+
+func blakeRound(state []uint32, message [16]uint32, sigma [16]uint32) []uint32 {
+	state[0], state[4], state[8], state[12] = mix(
+		state[0],
+		state[4],
+		state[8],
+		state[12],
+		message[sigma[0]],
+		message[sigma[1]],
+	)
+	state[1], state[5], state[9], state[13] = mix(
+		state[1],
+		state[5],
+		state[9],
+		state[13],
+		message[sigma[2]],
+		message[sigma[3]],
+	)
+	state[2], state[6], state[10], state[14] = mix(
+		state[2],
+		state[6],
+		state[10],
+		state[14],
+		message[sigma[4]],
+		message[sigma[5]],
+	)
+	state[3], state[7], state[11], state[15] = mix(
+		state[3],
+		state[7],
+		state[11],
+		state[15],
+		message[sigma[6]],
+		message[sigma[7]],
+	)
+	state[0], state[5], state[10], state[15] = mix(
+		state[0],
+		state[5],
+		state[10],
+		state[15],
+		message[sigma[8]],
+		message[sigma[9]],
+	)
+	state[1], state[6], state[11], state[12] = mix(
+		state[1],
+		state[6],
+		state[11],
+		state[12],
+		message[sigma[10]],
+		message[sigma[11]],
+	)
+	state[2], state[7], state[8], state[13] = mix(
+		state[2],
+		state[7],
+		state[8],
+		state[13],
+		message[sigma[12]],
+		message[sigma[13]],
+	)
+	state[3], state[4], state[9], state[14] = mix(
+		state[3],
+		state[4],
+		state[9],
+		state[14],
+		message[sigma[14]],
+		message[sigma[15]],
+	)
+	return state
+}
+
+func Blake2sCompress(h [8]uint32, message [16]uint32, t0 uint32, t1 uint32, f0 uint32, f1 uint32) []uint32 {
+	iv := IV()
+
+	state := make([]uint32, 0, 16)
+
+	state = append(state, h[:]...)
+	state = append(state, iv[:4]...)
+	state = append(state, iv[4]^t0, iv[5]^t1, iv[6]^f0, iv[7]^f1)
+
+	for _, sigmaList := range SIGMA() {
+		state = blakeRound(state, message, sigmaList)
+	}
+
+	newState := make([]uint32, 0, 8)
+	for i := 0; i < 8; i++ {
+		newState = append(newState, h[i]^state[i]^state[8+i])
+	}
+
+	return newState
 }
