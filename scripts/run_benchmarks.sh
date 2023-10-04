0e2901a4ed0a7619d44e062de935eeef97ef8a1c
Federica <fedemoletta@hotmail.com>
2023-10-04 12:04:57 -0300

    Add warmup

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -8,7 +8,7 @@ for file in $(ls $tests_path | grep .cairo | sed -E 's/\.cairo//'); do
 
     export PATH="$(pyenv root)/shims:$PATH"
 
-    hyperfine \
+    hyperfine -w 5 \
         -n "Cairo VM (CPython)" "PYENV_VERSION=3.9.15 cairo-run --proof_mode --memory_file /dev/null --trace_file /dev/null --layout starknet_with_keccak --program $tests_path/$file.json" \
 	    -n "Cairo VM (PyPy)" "PYENV_VERSION=pypy3.9-7.3.9 cairo-run --proof_mode --memory_file /dev/null --trace_file /dev/null --layout starknet_with_keccak --program $tests_path/$file.json" \
 	    -n "cairo-vm (Rust)" "cairo-vm/target/release/cairo-vm-cli $tests_path/$file.json --proof_mode --memory_file /dev/null --trace_file /dev/null --layout starknet_with_keccak" \
